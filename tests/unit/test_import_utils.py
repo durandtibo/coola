ccd98ee88579768d5656bb002578f240e08c6d32
Thibaut Durand <durand.tibo@gmail.com>
2023-01-13 08:18:02 -0800

    Reduce duplicate code for ndarrays

--- tests/unit/test_import_utils.py
+++ tests/unit/test_import_utils.py
@@ -2,7 +2,18 @@ from unittest.mock import patch
 
 from pytest import raises
 
-from coola.import_utils import check_torch
+from coola.import_utils import check_numpy, check_torch
+
+
+def test_check_numpy_with_package():
+    with patch("coola.import_utils.is_numpy_available", lambda *args: True):
+        check_numpy()
+
+
+def test_check_numpy_without_package():
+    with patch("coola.import_utils.is_numpy_available", lambda *args: False):
+        with raises(RuntimeError):
+            check_numpy()
 
 
 def test_check_torch_with_package():
