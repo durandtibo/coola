a3e372269dbcbcc9adff3d8f61084472b13b6ea7
Thibaut Durand <durand.tibo@gmail.com>
2023-06-10 20:45:59 -0700

    Add `SeriesAllCloseOperator`

--- src/coola/_pandas.py
+++ src/coola/_pandas.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import logging
 from typing import Any
 
+from coola.allclose import AllCloseTester, BaseAllCloseOperator, BaseAllCloseTester
 from coola.equality import BaseEqualityOperator, BaseEqualityTester, EqualityTester
 from coola.utils.imports import check_pandas, is_pandas_available
 
@@ -41,6 +42,41 @@ class DataFrameEqualityOperator(BaseEqualityOperator[DataFrame]):
         return object_equal
 
 
+class SeriesAllCloseOperator(BaseAllCloseOperator[Series]):
+    r"""Implements an equality operator for ``pandas.Series``."""
+
+    def __init__(self) -> None:
+        check_pandas()
+
+    def allclose(
+        self,
+        tester: BaseAllCloseTester,
+        object1: Series,
+        object2: Any,
+        rtol: float = 1e-5,
+        atol: float = 1e-8,
+        equal_nan: bool = False,
+        show_difference: bool = False,
+    ) -> bool:
+        if object1 is object2:
+            return True
+        if not isinstance(object2, Series):
+            if show_difference:
+                logger.info(f"object2 is not a pandas.Series: {type(object2)}")
+            return False
+        object_equal = tester.allclose(
+            object1.to_numpy(),
+            object2.to_numpy(),
+            rtol=rtol,
+            atol=atol,
+            equal_nan=equal_nan,
+            show_difference=show_difference,
+        )
+        if show_difference and not object_equal:
+            logger.info(f"pandas.Series are different\nobject1=\n{object1}\nobject2=\n{object2}")
+        return object_equal
+
+
 class SeriesEqualityOperator(BaseEqualityOperator[Series]):
     r"""Implements an equality operator for ``pandas.Series``."""
 
@@ -67,6 +103,9 @@ class SeriesEqualityOperator(BaseEqualityOperator[Series]):
 
 
 if is_pandas_available():  # pragma: no cover
+    if not AllCloseTester.has_allclose_operator(Series):
+        AllCloseTester.add_allclose_operator(Series, SeriesAllCloseOperator())
+
     if not EqualityTester.has_equality_operator(DataFrame):
         EqualityTester.add_equality_operator(DataFrame, DataFrameEqualityOperator())
     if not EqualityTester.has_equality_operator(Series):
