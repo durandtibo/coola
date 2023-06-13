769ca730f6d40e303d1a69be43efcd9651d558a8
Thibaut Durand <durand.tibo@gmail.com>
2023-06-13 08:27:38 -0700

    Add polars dependency

--- src/coola/utils/imports.py
+++ src/coola/utils/imports.py
@@ -2,9 +2,13 @@ from __future__ import annotations
 
 __all__ = [
     "check_numpy",
+    "check_pandas",
+    "check_polars",
     "check_torch",
     "check_xarray",
     "is_numpy_available",
+    "is_pandas_available",
+    "is_polars_available",
     "is_torch_available",
     "is_xarray_available",
 ]
@@ -32,7 +36,7 @@ def check_numpy() -> None:
 
 
 def is_pandas_available() -> bool:
-    r"""Indicates if the NumPy package is installed or not."""
+    r"""Indicates if the pandas package is installed or not."""
     return find_spec("pandas") is not None
 
 
@@ -47,6 +51,25 @@ def check_pandas() -> None:
             "`pandas` package is required but not installed. "
             "You can install `pandas` package with the command:\n\n"
             "pip install pandas\n"
+        )
+
+
+def is_polars_available() -> bool:
+    r"""Indicates if the polars package is installed or not."""
+    return find_spec("polars") is not None
+
+
+def check_polars() -> None:
+    r"""Checks if the polars package is installed.
+
+    Raises:
+        RuntimeError if the polars package is not installed.
+    """
+    if not is_polars_available():
+        raise RuntimeError(
+            "`polars` package is required but not installed. "
+            "You can install `polars` package with the command:\n\n"
+            "pip install polars\n"
         )
 
 
