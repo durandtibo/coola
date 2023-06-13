f8ac0322ef4ae109a95c004b459d8a86ebc426fa
Thibaut Durand <durand.tibo@gmail.com>
2023-06-13 13:21:08 -0700

    Add `SeriesEqualityOperator` (#139)

--- src/coola/__init__.py
+++ src/coola/__init__.py
@@ -23,6 +23,8 @@ from coola.equality import (
 )
 from coola.utils.imports import (
     is_numpy_available,
+    is_pandas_available,
+    is_polars_available,
     is_torch_available,
     is_xarray_available,
 )
@@ -30,6 +32,14 @@ from coola.utils.imports import (
 # Register NumPy comparators
 if is_numpy_available():  # pragma: no cover
     from coola import _numpy  # noqa: F401
+
+# Register pandas comparators
+if is_pandas_available():  # pragma: no cover
+    from coola import _pandas  # noqa: F401
+
+# Register polars comparators
+if is_polars_available():  # pragma: no cover
+    from coola import _polars  # noqa: F401
 
 # Register PyTorch comparators
 if is_torch_available():  # pragma: no cover
