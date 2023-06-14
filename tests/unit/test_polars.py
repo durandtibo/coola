9ae66aa5efb1ddf40a7e2edc1738fe021b455a19
Thibaut Durand <durand.tibo@gmail.com>
2023-06-13 23:51:48 -0700

    Add `SeriesAllCloseOperator`

--- tests/unit/test_polars.py
+++ tests/unit/test_polars.py
@@ -3,8 +3,17 @@ from unittest.mock import Mock
 
 from pytest import LogCaptureFixture
 
-from coola import EqualityTester, objects_are_equal
-from coola._polars import DataFrameEqualityOperator, SeriesEqualityOperator
+from coola import (
+    AllCloseTester,
+    EqualityTester,
+    objects_are_allclose,
+    objects_are_equal,
+)
+from coola._polars import (
+    DataFrameEqualityOperator,
+    SeriesAllCloseOperator,
+    SeriesEqualityOperator,
+)
 from coola.testing import polars_available
 from coola.utils.imports import is_polars_available
 
@@ -343,6 +352,205 @@ def test_dataframe_equality_operator_equal_false_different_type_show_difference(
             show_difference=True,
         )
         assert caplog.messages[0].startswith("object2 is not a polars.DataFrame")
+
+
+############################################
+#     Tests for SeriesAllCloseOperator     #
+############################################
+
+
+@polars_available
+def test_objects_are_allclose_series() -> None:
+    assert objects_are_allclose(polars.Series([1, 2, 3, 4, 5]), polars.Series([1, 2, 3, 4, 5]))
+
+
+@polars_available
+def test_series_allclose_operator_str() -> None:
+    assert str(SeriesAllCloseOperator()).startswith("SeriesAllCloseOperator(")
+
+
+@polars_available
+def test_series_allclose_operator_allclose_true_int() -> None:
+    assert SeriesAllCloseOperator().allclose(
+        AllCloseTester(), polars.Series([1, 2, 3, 4, 5]), polars.Series([1, 2, 3, 4, 5])
+    )
+
+
+@polars_available
+def test_series_allclose_operator_allclose_true_float() -> None:
+    assert SeriesAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.Series([1.0, 2.0, 3.0, 4.0, 5.0]),
+        polars.Series([1.0, 2.0, 3.0, 4.0, 5.0]),
+    )
+
+
+@polars_available
+def test_series_allclose_operator_allclose_true_str() -> None:
+    assert SeriesAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.Series(["a", "b", "c", "d", "e"]),
+        polars.Series(["a", "b", "c", "d", "e"]),
+    )
+
+
+@polars_available
+def test_series_allclose_operator_allclose_true_datetime() -> None:
+    assert SeriesAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.Series(["2020/10/12", "2021/3/14", "2022/4/14"]).str.to_datetime(),
+        polars.Series(["2020/10/12", "2021/3/14", "2022/4/14"]).str.to_datetime(),
+    )
+
+
+@polars_available
+def test_series_allclose_operator_allclose_true_same_object() -> None:
+    obj = polars.Series([1, 2, 3, 4, 5])
+    assert SeriesAllCloseOperator().allclose(AllCloseTester(), obj, obj)
+
+
+@polars_available
+def test_series_allclose_operator_allclose_true_show_difference(caplog: LogCaptureFixture) -> None:
+    with caplog.at_level(logging.INFO):
+        assert SeriesAllCloseOperator().allclose(
+            AllCloseTester(),
+            polars.Series([1, 2, 3, 4, 5]),
+            polars.Series([1, 2, 3, 4, 5]),
+            show_difference=True,
+        )
+        assert not caplog.messages
+
+
+@polars_available
+def test_series_allclose_operator_allclose_false_different_data() -> None:
+    assert not SeriesAllCloseOperator().allclose(
+        AllCloseTester(), polars.Series([1, 2, 3, 4, 5]), polars.Series(["a", "b", "c", "d", "e"])
+    )
+
+
+@polars_available
+def test_series_allclose_operator_allclose_false_different_dtype() -> None:
+    assert not SeriesAllCloseOperator().allclose(
+        AllCloseTester(), polars.Series([1, 2, 3, 4, 5]), polars.Series([1.0, 2.0, 3.0, 4.0, 5.0])
+    )
+
+
+@polars_available
+def test_series_allclose_operator_allclose_false_nan() -> None:
+    assert not SeriesAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.Series([1.0, 2.0, 3.0, 4.0, float("nan")]),
+        polars.Series([1.0, 2.0, 3.0, 4.0, float("nan")]),
+    )
+
+
+@polars_available
+def test_series_allclose_operator_allclose_true_nan() -> None:
+    assert SeriesAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.Series([1.0, 2.0, 3.0, 4.0, float("nan")]),
+        polars.Series([1.0, 2.0, 3.0, 4.0, float("nan")]),
+        equal_nan=True,
+    )
+
+
+@polars_available
+def test_series_allclose_operator_allclose_false_nat() -> None:
+    assert not SeriesAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.Series(["2020/10/12", "2021/3/14", "2022/4/14", None]).str.to_datetime(),
+        polars.Series(["2020/10/12", "2021/3/14", "2022/4/14", None]).str.to_datetime(),
+    )
+
+
+@polars_available
+def test_series_allclose_operator_allclose_true_nat() -> None:
+    assert SeriesAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.Series(["2020/10/12", "2021/3/14", "2022/4/14", None]).str.to_datetime(),
+        polars.Series(["2020/10/12", "2021/3/14", "2022/4/14", None]).str.to_datetime(),
+        equal_nan=True,
+    )
+
+
+@polars_available
+def test_series_allclose_operator_allclose_false_none() -> None:
+    assert not SeriesAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.Series(["a", "b", "c", "d", "e", None]),
+        polars.Series(["a", "b", "c", "d", "e", None]),
+    )
+
+
+@polars_available
+def test_series_allclose_operator_allclose_true_none() -> None:
+    assert SeriesAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.Series(["a", "b", "c", "d", "e", None]),
+        polars.Series(["a", "b", "c", "d", "e", None]),
+        equal_nan=True,
+    )
+
+
+@polars_available
+def test_series_allclose_operator_allclose_false_show_difference(caplog: LogCaptureFixture) -> None:
+    with caplog.at_level(logging.INFO):
+        assert not SeriesAllCloseOperator().allclose(
+            AllCloseTester(),
+            polars.Series([1, 2, 3, 4, 5]),
+            polars.Series(["a", "b", "c", "d", "e"]),
+            show_difference=True,
+        )
+        assert caplog.messages[-1].startswith("polars.Series are different")
+
+
+@polars_available
+def test_series_allclose_operator_allclose_false_different_type() -> None:
+    assert not SeriesAllCloseOperator().allclose(
+        AllCloseTester(), polars.Series([1, 2, 3, 4, 5]), "meow"
+    )
+
+
+@polars_available
+def test_series_allclose_operator_allclose_false_different_type_show_difference(
+    caplog: LogCaptureFixture,
+) -> None:
+    with caplog.at_level(logging.INFO):
+        assert not SeriesAllCloseOperator().allclose(
+            AllCloseTester(), polars.Series([1, 2, 3, 4, 5]), "meow", show_difference=True
+        )
+        assert caplog.messages[0].startswith("object2 is not a polars.Series")
+
+
+# TODO: Commented because of https://github.com/pola-rs/polars/issues/9358
+# @polars_available
+# @mark.parametrize(
+#     "series,atol",
+#     (
+#         (polars.Series([1.5, 1.5, 1.5]), 1.0),
+#         (polars.Series([1.05, 1.05, 1.05]), 1e-1),
+#         (polars.Series([1.005, 1.005, 1.005]), 1e-2),
+#     ),
+# )
+# def test_series_allclose_operator_allclose_true_atol(series: polars.Series, atol: float) -> None:
+#     assert SeriesAllCloseOperator().allclose(
+#         AllCloseTester(), polars.Series([1.0, 1.0, 1.0]), series, atol=atol, rtol=0.0
+#     )
+#
+#
+# @polars_available
+# @mark.parametrize(
+#     "series,rtol",
+#     (
+#         (polars.Series([1.5, 1.5, 1.5]), 1.0),
+#         (polars.Series([1.05, 1.05, 1.05]), 1e-1),
+#         (polars.Series([1.005, 1.005, 1.005]), 1e-2),
+#     ),
+# )
+# def test_series_allclose_operator_allclose_true_rtol(series: polars.Series, rtol: float) -> None:
+#     assert SeriesAllCloseOperator().allclose(
+#         AllCloseTester(), polars.Series([1.0, 1.0, 1.0]), series, rtol=rtol
+#     )
 
 
 ############################################
