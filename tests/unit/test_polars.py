8e9053306d86fb9a935e413493a43c60f188add2
Thibaut Durand <durand.tibo@gmail.com>
2023-06-14 20:23:18 -0700

    Add `DataFrameAllCloseOperator` for `polars` (#143)

--- tests/unit/test_polars.py
+++ tests/unit/test_polars.py
@@ -10,6 +10,7 @@ from coola import (
     objects_are_equal,
 )
 from coola._polars import (
+    DataFrameAllCloseOperator,
     DataFrameEqualityOperator,
     SeriesAllCloseOperator,
     SeriesEqualityOperator,
@@ -24,9 +25,404 @@ else:
 
 
 @polars_available
+def test_allclose_tester_registry() -> None:
+    assert isinstance(AllCloseTester.registry[polars.DataFrame], DataFrameAllCloseOperator)
+    assert isinstance(AllCloseTester.registry[polars.Series], SeriesAllCloseOperator)
+
+
+@polars_available
 def test_equality_tester_registry() -> None:
     assert isinstance(EqualityTester.registry[polars.DataFrame], DataFrameEqualityOperator)
     assert isinstance(EqualityTester.registry[polars.Series], SeriesEqualityOperator)
+
+
+###############################################
+#     Tests for DataFrameAllCloseOperator     #
+###############################################
+
+
+@polars_available
+def test_objects_are_allclose_dataframe() -> None:
+    assert objects_are_allclose(
+        polars.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [1.1, 2.2, 3.3, 4.4, 5.5],
+                "col3": ["a", "b", "c", "d", "e"],
+                "col4": polars.Series(
+                    ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+                ).str.to_datetime(),
+            }
+        ),
+        polars.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [1.1, 2.2, 3.3, 4.4, 5.5],
+                "col3": ["a", "b", "c", "d", "e"],
+                "col4": polars.Series(
+                    ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+                ).str.to_datetime(),
+            }
+        ),
+    )
+
+
+@polars_available
+def test_dataframe_allclose_operator_str() -> None:
+    assert str(DataFrameAllCloseOperator()).startswith("DataFrameAllCloseOperator(")
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_true() -> None:
+    assert DataFrameAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [1.1, 2.2, 3.3, 4.4, 5.5],
+                "col3": ["a", "b", "c", "d", "e"],
+                "col4": polars.Series(
+                    ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+                ).str.to_datetime(),
+            }
+        ),
+        polars.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [1.1, 2.2, 3.3, 4.4, 5.5],
+                "col3": ["a", "b", "c", "d", "e"],
+                "col4": polars.Series(
+                    ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+                ).str.to_datetime(),
+            }
+        ),
+    )
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_true_same_object() -> None:
+    obj = polars.DataFrame(
+        {
+            "col1": [1, 2, 3, 4, 5],
+            "col2": [1.1, 2.2, 3.3, 4.4, 5.5],
+            "col3": ["a", "b", "c", "d", "e"],
+            "col4": polars.Series(
+                ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+            ).str.to_datetime(),
+        }
+    )
+    assert DataFrameAllCloseOperator().allclose(AllCloseTester(), obj, obj)
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_true_show_difference(
+    caplog: LogCaptureFixture,
+) -> None:
+    with caplog.at_level(logging.INFO):
+        assert DataFrameAllCloseOperator().allclose(
+            AllCloseTester(),
+            polars.DataFrame(
+                {
+                    "col1": [1, 2, 3, 4, 5],
+                    "col2": [1.1, 2.2, 3.3, 4.4, 5.5],
+                    "col3": ["a", "b", "c", "d", "e"],
+                    "col4": polars.Series(
+                        ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+                    ).str.to_datetime(),
+                }
+            ),
+            polars.DataFrame(
+                {
+                    "col1": [1, 2, 3, 4, 5],
+                    "col2": [1.1, 2.2, 3.3, 4.4, 5.5],
+                    "col3": ["a", "b", "c", "d", "e"],
+                    "col4": polars.Series(
+                        ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+                    ).str.to_datetime(),
+                }
+            ),
+            show_difference=True,
+        )
+        assert not caplog.messages
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_false_different_data() -> None:
+    assert not DataFrameAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [1.1, 2.2, 3.3, 4.4, 5.5],
+                "col3": ["a", "b", "c", "d", "e"],
+                "col4": polars.Series(
+                    ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+                ).str.to_datetime(),
+            }
+        ),
+        polars.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [1.1, 2.2, 3.3, 4.4, 5.5],
+                "col3": ["a", "b", "c", "d", "e"],
+            }
+        ),
+    )
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_false_different_columns() -> None:
+    assert not DataFrameAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [1.1, 2.2, 3.3, 4.4, 5.5],
+                "col3": ["a", "b", "c", "d", "e"],
+                "col4": polars.Series(
+                    ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+                ).str.to_datetime(),
+            }
+        ),
+        polars.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col": [1.1, 2.2, 3.3, 4.4, 5.5],
+                "col3": ["a", "b", "c", "d", "e"],
+                "col4": polars.Series(
+                    ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+                ).str.to_datetime(),
+            }
+        ),
+    )
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_false_different_dtype() -> None:
+    assert not DataFrameAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [1.1, 2.2, 3.3, 4.4, 5.5],
+                "col3": ["a", "b", "c", "d", "e"],
+                "col4": polars.Series(
+                    ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+                ).str.to_datetime(),
+            }
+        ),
+        polars.DataFrame(
+            {
+                "col1": [1.0, 2.0, 3.0, 4.0, 5.0],
+                "col2": [1.1, 2.2, 3.3, 4.4, 5.5],
+                "col3": ["a", "b", "c", "d", "e"],
+                "col4": polars.Series(
+                    ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+                ).str.to_datetime(),
+            }
+        ),
+    )
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_false_null() -> None:
+    assert not DataFrameAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5, None],
+                "col2": [1.1, 2.2, 3.3, 4.4, 5.5, float("nan")],
+                "col3": ["a", "b", "c", "d", "e", None],
+                "col4": polars.Series(
+                    ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16", None]
+                ).str.to_datetime(),
+            }
+        ),
+        polars.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5, None],
+                "col2": [1.1, 2.2, 3.3, 4.4, 5.5, float("nan")],
+                "col3": ["a", "b", "c", "d", "e", None],
+                "col4": polars.Series(
+                    ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16", None]
+                ).str.to_datetime(),
+            }
+        ),
+    )
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_true_null() -> None:
+    assert DataFrameAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5, None],
+                "col2": [1.1, 2.2, 3.3, 4.4, 5.5, float("nan")],
+                "col3": ["a", "b", "c", "d", "e", None],
+                "col4": polars.Series(
+                    ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16", None]
+                ).str.to_datetime(),
+            }
+        ),
+        polars.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5, None],
+                "col2": [1.1, 2.2, 3.3, 4.4, 5.5, float("nan")],
+                "col3": ["a", "b", "c", "d", "e", None],
+                "col4": polars.Series(
+                    ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16", None]
+                ).str.to_datetime(),
+            }
+        ),
+        equal_nan=True,
+    )
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_false_nan() -> None:
+    assert not DataFrameAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.DataFrame({"col": [1.1, 2.2, 3.3, 4.4, 5.5, float("nan")]}),
+        polars.DataFrame({"col": [1.1, 2.2, 3.3, 4.4, 5.5, float("nan")]}),
+    )
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_false_nat() -> None:
+    assert not DataFrameAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.DataFrame(
+            {"col": polars.Series(["2020/10/12", "2021/3/14", "2022/4/14", None]).str.to_datetime()}
+        ),
+        polars.DataFrame(
+            {"col": polars.Series(["2020/10/12", "2021/3/14", "2022/4/14", None]).str.to_datetime()}
+        ),
+    )
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_true_nat() -> None:
+    assert DataFrameAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.DataFrame(
+            {"col": polars.Series(["2020/10/12", "2021/3/14", "2022/4/14", None]).str.to_datetime()}
+        ),
+        polars.DataFrame(
+            {"col": polars.Series(["2020/10/12", "2021/3/14", "2022/4/14", None]).str.to_datetime()}
+        ),
+        equal_nan=True,
+    )
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_false_none_str() -> None:
+    assert not DataFrameAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.DataFrame({"col": ["a", "b", "c", "d", "e", None]}),
+        polars.DataFrame({"col": ["a", "b", "c", "d", "e", None]}),
+    )
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_true_none_str() -> None:
+    assert DataFrameAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.DataFrame({"col": ["a", "b", "c", "d", "e", None]}),
+        polars.DataFrame({"col": ["a", "b", "c", "d", "e", None]}),
+        equal_nan=True,
+    )
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_false_none_int() -> None:
+    assert not DataFrameAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.DataFrame({"col": [1, 2, 3, 4, 5, None]}),
+        polars.DataFrame({"col": [1, 2, 3, 4, 5, None]}),
+    )
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_true_none_int() -> None:
+    assert DataFrameAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.DataFrame({"col": [1, 2, 3, 4, 5, None]}),
+        polars.DataFrame({"col": [1, 2, 3, 4, 5, None]}),
+        equal_nan=True,
+    )
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_false_show_difference(
+    caplog: LogCaptureFixture,
+) -> None:
+    with caplog.at_level(logging.INFO):
+        assert not DataFrameAllCloseOperator().allclose(
+            AllCloseTester(),
+            polars.DataFrame(
+                {
+                    "col1": [1, 2, 3, 4, 5],
+                    "col2": [1.1, 2.2, 3.3, 4.4, 5.5],
+                    "col3": ["a", "b", "c", "d", "e"],
+                    "col4": polars.Series(
+                        ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+                    ).str.to_datetime(),
+                }
+            ),
+            polars.DataFrame(
+                {
+                    "col1": [1, 2, 3, 4, 5],
+                    "col3": ["a", "b", "c", "d", "e"],
+                    "col4": polars.Series(
+                        ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+                    ).str.to_datetime(),
+                }
+            ),
+            show_difference=True,
+        )
+        assert caplog.messages[-1].startswith("polars.DataFrames are different")
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_false_different_type() -> None:
+    assert not DataFrameAllCloseOperator().allclose(
+        AllCloseTester(),
+        polars.DataFrame(
+            {
+                "col1": [1, 2, 3, 4, 5],
+                "col2": [1.1, 2.2, 3.3, 4.4, 5.5],
+                "col3": ["a", "b", "c", "d", "e"],
+                "col4": polars.Series(
+                    ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+                ).str.to_datetime(),
+            }
+        ),
+        "meow",
+    )
+
+
+@polars_available
+def test_dataframe_allclose_operator_allclose_false_different_type_show_difference(
+    caplog: LogCaptureFixture,
+) -> None:
+    with caplog.at_level(logging.INFO):
+        assert not DataFrameAllCloseOperator().allclose(
+            AllCloseTester(),
+            polars.DataFrame(
+                {
+                    "col1": [1, 2, 3, 4, 5],
+                    "col2": [1.1, 2.2, 3.3, 4.4, 5.5],
+                    "col3": ["a", "b", "c", "d", "e"],
+                    "col4": polars.Series(
+                        ["2020/10/12", "2021/3/14", "2022/4/14", "2023/5/15", "2024/6/16"]
+                    ).str.to_datetime(),
+                }
+            ),
+            "meow",
+            show_difference=True,
+        )
+        assert caplog.messages[-1].startswith("object2 is not a polars.DataFrame")
 
 
 ###############################################
