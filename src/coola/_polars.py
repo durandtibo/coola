d04cf7b05f695c5e87c610770ae4e4493594a17d
Thibaut Durand <durand.tibo@gmail.com>
2023-06-16 11:51:23 -0700

    Add `nulls_compare_equal` to `DataFrameEqualityOperator`

--- src/coola/_polars.py
+++ src/coola/_polars.py
@@ -60,10 +60,19 @@ class DataFrameAllCloseOperator(BaseAllCloseOperator[DataFrame]):
 
 
 class DataFrameEqualityOperator(BaseEqualityOperator[DataFrame]):
-    r"""Implements an equality operator for ``polars.DataFrame``."""
+    r"""Implements an equality operator for ``polars.DataFrame``.
 
-    def __init__(self) -> None:
+    Args:
+        nulls_compare_equal (bool, optional): If ``True``, null values
+            (e.g. NaN or NaT) compare as true. Default: ``False``
+    """
+
+    def __init__(self, nulls_compare_equal: bool = False) -> None:
         check_polars()
+        self._nulls_compare_equal = bool(nulls_compare_equal)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__}(nulls_compare_equal={self._nulls_compare_equal})"
 
     def equal(
         self,
@@ -78,15 +87,35 @@ class DataFrameEqualityOperator(BaseEqualityOperator[DataFrame]):
             if show_difference:
                 logger.info(f"object2 is not a polars.DataFrame: {type(object2)}")
             return False
-        try:
-            assert_frame_equal(object1, object2, check_exact=True, nans_compare_equal=False)
-            object_equal = object1.null_count().sum(axis=1).to_list()[0] == 0
-        except AssertionError:
-            object_equal = False
+        object_equal = self._compare_dataframes(object1, object2)
         if show_difference and not object_equal:
             logger.info(
                 f"polars.DataFrames are different\nobject1=\n{object1}\nobject2=\n{object2}"
             )
+        return object_equal
+
+    def _compare_dataframes(self, df1: DataFrame, df2: DataFrame) -> bool:
+        r"""Indicates if the two DataFrames are equal or not.
+
+        Args:
+            df1 (``polars.DataFrame``): Specifies the first
+                DataFrame to compare.
+            df2 (``polars.DataFrame``): Specifies the second
+                DataFrame to compare.
+
+        Returns:
+            bool: ``True``if the two series are equal,
+                otherwise ``False``.
+        """
+        try:
+            assert_frame_equal(
+                df1, df2, check_exact=True, nans_compare_equal=self._nulls_compare_equal
+            )
+            object_equal = True
+        except AssertionError:
+            object_equal = False
+        if object_equal and not self._nulls_compare_equal:
+            object_equal = df1.null_count().sum(axis=1).to_list()[0] == 0
         return object_equal
 
 
