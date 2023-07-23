ff5b36834a1a7107ca1cc105037d959fca583931
Thibaut Durand <durand.tibo@gmail.com>
2023-07-22 19:15:33 -0700

    Minor cleaning (#209)

--- src/coola/testers/__init__.py
+++ src/coola/testers/__init__.py
@@ -11,5 +11,3 @@ __all__ = [
 from coola.testers.allclose import AllCloseTester, LocalAllCloseTester
 from coola.testers.base import BaseAllCloseTester, BaseEqualityTester
 from coola.testers.equality import EqualityTester, LocalEqualityTester
-
-# from coola.testers import registry  # noqa: F401
