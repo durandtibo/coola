4ef4b5569d5232ff9a960afa8709dcee3e926f67
Thibaut Durand <durand.tibo@gmail.com>
2023-10-12 20:24:36 -0700

    Add `repr_mapping`

--- src/coola/utils/format.py
+++ src/coola/utils/format.py
@@ -1,13 +1,20 @@
 from __future__ import annotations
 
-__all__ = ["repr_indent", "repr_indent", "str_indent", "str_mapping", "str_sequence"]
+__all__ = [
+    "repr_indent",
+    "repr_mapping",
+    "repr_sequence",
+    "str_indent",
+    "str_mapping",
+    "str_sequence",
+]
 
 from collections.abc import Mapping, Sequence
 from typing import Any
 
 
 def repr_indent(original: Any, num_spaces: int = 2) -> str:
-    r"""Add indentations if the original string is a multi-lines string.
+    r"""Adds indentations if the original string is a multi-lines string.
 
     Args:
     ----
@@ -40,6 +47,42 @@ def repr_indent(original: Any, num_spaces: int = 2) -> str:
     return str_indent(original, num_spaces)
 
 
+def repr_mapping(mapping: Mapping, sorted_keys: bool = False, num_spaces: int = 2) -> str:
+    r"""Computes a string representation of a mapping.
+
+    This function was designed for flat dictionary. If you have a
+    nested dictionary, you may consider other functions. Note that
+    this function works for nested dict but the output may not be
+    nice.
+
+    Args:
+    ----
+        mapping (``Mapping``): Specifies the mapping.
+        sorted_keys (bool, optional): Specifies if the key of the dict
+            are sorted or not. Default: ``False``
+        num_spaces (int, optional): Specifies the number of spaces
+            used for the indentation. Default: ``2``.
+
+    Returns:
+    -------
+        str: The string representation of the mapping.
+
+    Example usage:
+
+    .. code-block:: pycon
+
+        >>> from coola.utils.format import repr_mapping
+        >>> print(repr_mapping({"key1": "abc", "key2": "something\nelse"}))
+        (key1): abc
+        (key2): something
+          else
+    """
+    lines = []
+    for key, value in sorted(mapping.items()) if sorted_keys else mapping.items():
+        lines.append(f"({key}): {repr_indent(value, num_spaces=num_spaces)}")
+    return "\n".join(lines)
+
+
 def repr_sequence(sequence: Sequence, num_spaces: int = 2) -> str:
     r"""Computes a string representation of a sequence.
 
@@ -70,7 +113,7 @@ def repr_sequence(sequence: Sequence, num_spaces: int = 2) -> str:
 
 
 def str_indent(original: Any, num_spaces: int = 2) -> str:
-    r"""Add indentations if the original string is a multi-lines string.
+    r"""Adds indentations if the original string is a multi-lines string.
 
     Args:
     ----
