7a650f5a8fec7f9dfb32c4c22d3fff6d8a6fecad
Thibaut Durand <durand.tibo@gmail.com>
2024-01-05 22:37:41 -0800

    Add `DefaultEqualityComparator` (#359)

---
+++ src/coola/equality/comparators/__init__.py
@@ -0,0 +1,8 @@
+r"""Contain the comparators to check if two objects are equal or not."""
+
+from __future__ import annotations
+
+__all__ = ["BaseEqualityComparator", "DefaultEqualityComparator"]
+
+from coola.equality.comparators.base import BaseEqualityComparator
+from coola.equality.comparators.default import DefaultEqualityComparator
---
+++ src/coola/equality/comparators/default.py
@@ -0,0 +1,56 @@
+r"""Implement the default equality comparator."""
+
+from __future__ import annotations
+
+__all__ = ["DefaultEqualityComparator"]
+
+import logging
+from typing import TYPE_CHECKING, Any
+
+from coola.equality.comparators.base import BaseEqualityComparator
+from coola.equality.handlers import (
+    ObjectEqualHandler,
+    SameObjectHandler,
+    SameTypeHandler,
+)
+
+if TYPE_CHECKING:
+    from coola.equality import EqualityConfig
+
+logger = logging.getLogger(__name__)
+
+
+class DefaultEqualityComparator(BaseEqualityComparator[Any]):
+    r"""Implement a default equality comparator.
+
+    The ``==`` operator is used to test the equality between the
+    objects.
+
+    Example usage:
+
+    ```pycon
+    >>> from coola.equality import EqualityConfig
+    >>> from coola.equality.comparators import DefaultEqualityComparator
+    >>> from coola.testers import EqualityTester
+    >>> config = EqualityConfig(tester=EqualityTester())
+    >>> comparator = DefaultEqualityComparator()
+    >>> comparator.equal(42, 42, config)
+    True
+    >>> comparator.equal("meow", "meov", config)
+    False
+
+    ```
+    """
+
+    def __init__(self) -> None:
+        self._handler = SameObjectHandler()
+        self._handler.chain(SameTypeHandler()).chain(ObjectEqualHandler())
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, self.__class__)
+
+    def clone(self) -> DefaultEqualityComparator:
+        return self.__class__()
+
+    def equal(self, object1: Any, object2: Any, config: EqualityConfig) -> bool:
+        return self._handler.handle(object1=object1, object2=object2, config=config)
