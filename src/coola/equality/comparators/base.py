e656b1a7985cc56f4cec69f850c2c8d156929bdf
Thibaut Durand <durand.tibo@gmail.com>
2024-01-05 22:16:36 -0800

    Add `DefaultEqualityComparator`

---
+++ src/coola/equality/comparators/base.py
@@ -0,0 +1,88 @@
+r"""Define the equality comparator base class."""
+
+from __future__ import annotations
+
+__all__ = ["BaseEqualityComparator"]
+
+import logging
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Any, Generic, TypeVar
+
+if TYPE_CHECKING:
+    from coola.equality.config import EqualityConfig
+
+logger = logging.getLogger(__name__)
+
+T = TypeVar("T")
+
+
+class BaseEqualityComparator(ABC, Generic[T]):
+    r"""Define the base class to implement an equality operator.
+
+    Example usage:
+
+    ```pycon
+    >>> from coola.equality import EqualityConfig
+    >>> from coola.equality.comparators import DefaultEqualityComparator
+    >>> from coola.testers import EqualityTester
+    >>> config = EqualityConfig(tester=EqualityTester())
+    >>> comparator = DefaultEqualityComparator()
+    >>> comparator.equal(42, 42, config)
+    True
+    >>> comparator.equal("meow", "meov", config)
+    False
+
+    ```
+    """
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__}()"
+
+    @abstractmethod
+    def clone(self) -> BaseEqualityComparator:
+        r"""Return a copy of the equality operator.
+
+        Returns:
+            A copy of the equality operator.
+
+        Example usage:
+
+        ```pycon
+        >>> from coola.equality.comparators import DefaultEqualityComparator
+        >>> op = DefaultEqualityComparator()
+        >>> op_cloned = op.clone()
+        >>> op_cloned
+        DefaultEqualityComparator()
+        >>> op is op_cloned
+        False
+
+        ```
+        """
+
+    @abstractmethod
+    def equal(self, object1: T, object2: Any, config: EqualityConfig) -> bool:
+        r"""Indicate if two objects are equal or not.
+
+        Args:
+            object1: Specifies the first object to compare.
+            object2: Specifies the second object to compare.
+            config: Specifies the equality configuration.
+
+        Returns:
+            ``True`` if the two objects are equal, otherwise ``False``.
+
+        Example usage:
+
+        ```pycon
+        >>> from coola.equality import EqualityConfig
+        >>> from coola.equality.comparators import DefaultEqualityComparator
+        >>> from coola.testers import EqualityTester
+        >>> config = EqualityConfig(tester=EqualityTester())
+        >>> comparator = DefaultEqualityComparator()
+        >>> comparator.equal(42, 42, config)
+        True
+        >>> comparator.equal("meow", "meov", config)
+        False
+
+        ```
+        """
---
+++ tests/unit/equality/comparators/test_default.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+
+import logging
+from unittest.mock import Mock
+
+import pytest
+
+from coola import EqualityTester
+from coola.equality import EqualityConfig
+from coola.equality.comparators import DefaultEqualityComparator
+
+
+@pytest.fixture()
+def config() -> EqualityConfig:
+    return EqualityConfig(tester=EqualityTester())
+
+
+###############################################
+#     Tests for DefaultEqualityComparator     #
+###############################################
+
+
+def test_default_equality_comparator_str() -> None:
+    assert str(DefaultEqualityComparator()) == "DefaultEqualityComparator()"
+
+
+def test_default_equality_comparator__eq__true() -> None:
+    assert DefaultEqualityComparator() == DefaultEqualityComparator()
+
+
+def test_default_equality_comparator__eq__false() -> None:
+    assert DefaultEqualityComparator() != 123
+
+
+def test_default_equality_comparator_clone() -> None:
+    op = DefaultEqualityComparator()
+    op_cloned = op.clone()
+    assert op is not op_cloned
+    assert op == op_cloned
+
+
+@pytest.mark.parametrize(
+    ("object1", "object2"),
+    [
+        (1, 1),
+        (0, 0),
+        (-1, -1),
+        (1.0, 1.0),
+        (0.0, 0.0),
+        (-1.0, -1.0),
+        (True, True),
+        (False, False),
+        (None, None),
+    ],
+)
+def test_default_equality_comparator_equal_true_scalar(
+    object1: bool | float | None, object2: bool | float | None, config: EqualityConfig
+) -> None:
+    assert DefaultEqualityComparator().equal(object1, object2, config)
+
+
+def test_default_equality_comparator_equal_true_same_object(config: EqualityConfig) -> None:
+    obj = Mock()
+    assert DefaultEqualityComparator().equal(obj, obj, config)
+
+
+@pytest.mark.parametrize(
+    ("object1", "object2"),
+    [
+        (1, 2),
+        (1.0, 2.0),
+        (True, False),
+        (1, 1.0),
+        (1, True),
+        (1.0, True),
+        (1.0, None),
+    ],
+)
+def test_default_equality_comparator_equal_false_scalar(
+    object1: bool | float, object2: bool | float | None, config: EqualityConfig
+) -> None:
+    assert not DefaultEqualityComparator().equal(object1, object2, config)
+
+
+def test_default_equality_comparator_equal_different_value_show_difference(
+    caplog: pytest.LogCaptureFixture, config: EqualityConfig
+) -> None:
+    config.show_difference = True
+    comparator = DefaultEqualityComparator()
+    with caplog.at_level(logging.INFO):
+        assert not comparator.equal(object1=1, object2=2, config=config)
+        assert caplog.messages[0].startswith("objects are different:")
+
+
+def test_default_equality_comparator_equal_different_type(config: EqualityConfig) -> None:
+    assert not DefaultEqualityComparator().equal(object1=[], object2=(), config=config)
+
+
+def test_default_equality_comparator_equal_different_type_show_difference(
+    caplog: pytest.LogCaptureFixture, config: EqualityConfig
+) -> None:
+    config.show_difference = True
+    comparator = DefaultEqualityComparator()
+    with caplog.at_level(logging.INFO):
+        assert not comparator.equal(object1=[], object2=(), config=config)
+        assert caplog.messages[0].startswith("objects have different types:")
