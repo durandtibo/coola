23baee9ccc2c4a5c730d3ed49623618032261b1d
Thibaut Durand <durand.tibo@gmail.com>
2024-01-06 12:11:16 -0800

    Add `TensorEqualityComparator`

--- tests/unit/equality/comparators/test_utils.py
+++ tests/unit/equality/comparators/test_utils.py
@@ -3,13 +3,17 @@ from __future__ import annotations
 from coola.equality.comparators import (
     ArrayEqualityComparator,
     DefaultEqualityComparator,
+    TensorEqualityComparator,
     get_type_comparator_mapping,
 )
-from coola.utils.imports import is_numpy_available, numpy_available
+from coola.testing import torch_available
+from coola.utils.imports import is_numpy_available, is_torch_available, numpy_available
 
 if is_numpy_available():
     import numpy as np
 
+if is_torch_available():
+    import torch
 
 #################################################
 #     Tests for get_type_comparator_mapping     #
@@ -26,3 +30,9 @@ def test_get_type_comparator_mapping() -> None:
 def test_get_type_comparator_mapping_numpy() -> None:
     mapping = get_type_comparator_mapping()
     assert isinstance(mapping[np.ndarray], ArrayEqualityComparator)
+
+
+@torch_available
+def test_get_type_comparator_mapping_torch() -> None:
+    mapping = get_type_comparator_mapping()
+    assert isinstance(mapping[torch.Tensor], TensorEqualityComparator)
