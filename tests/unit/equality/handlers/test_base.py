21004b44d332ed9c8cd7b52be695f7793777296c
Thibaut Durand <durand.tibo@gmail.com>
2024-01-05 21:47:54 -0800

    Add `chain` method to `BaseEqualityHandler`

---
+++ tests/unit/equality/handlers/test_base.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+import pytest
+
+from coola import EqualityTester
+from coola.equality import EqualityConfig
+from coola.equality.handlers import (
+    ObjectEqualHandler,
+    SameObjectHandler,
+    SameTypeHandler,
+    TrueHandler,
+)
+
+
+@pytest.fixture()
+def config() -> EqualityConfig:
+    return EqualityConfig(tester=EqualityTester())
+
+
+def test_chain_1(config: EqualityConfig) -> None:
+    handler = SameObjectHandler()
+    handler.chain(TrueHandler())
+    assert handler.next_handler == TrueHandler()
+    assert handler.handle(object1=[1, 2, 3], object2=[1, 2, 3], config=config)
+
+
+def test_chain_multiple(config: EqualityConfig) -> None:
+    handler = SameObjectHandler()
+    handler.chain(SameTypeHandler()).chain(ObjectEqualHandler())
+    assert handler.next_handler == SameTypeHandler()
+    assert handler.handle(object1=[1, 2, 3], object2=[1, 2, 3], config=config)
