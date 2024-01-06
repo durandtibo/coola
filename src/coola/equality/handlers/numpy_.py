4286a557ddf758b7eec15dc3fe76e0d2fe4d113c
Thibaut Durand <durand.tibo@gmail.com>
2024-01-05 23:49:14 -0800

    Add `ArrayEqualHandler`

---
+++ src/coola/equality/handlers/numpy_.py
@@ -0,0 +1,71 @@
+r"""Implement some handlers for ``numpy.ndarray``s."""
+
+from __future__ import annotations
+
+__all__ = ["ArrayEqualHandler"]
+
+import logging
+from typing import TYPE_CHECKING
+from unittest.mock import Mock
+
+from coola.equality.handlers.base import BaseEqualityHandler
+from coola.utils import is_numpy_available
+
+if is_numpy_available():
+    import numpy as np
+else:  # pragma: no cover
+    np = Mock()
+
+if TYPE_CHECKING:
+    from coola.equality.config import EqualityConfig
+
+logger = logging.getLogger(__name__)
+
+
+class ArrayEqualHandler(BaseEqualityHandler):
+    r"""Check if the two arrays are equal.
+
+    This handler returns ``True`` if the two arrays are equal,
+    otherwise ``False``. This handler is designed to be used at
+    the end of the chain of responsibility. This handler does
+    not call the next handler.
+
+    Example usage:
+
+    ```pycon
+    >>> import numpy as np
+    >>> from coola.equality import EqualityConfig
+    >>> from coola.equality.handlers.numpy_ import ArrayEqualHandler
+    >>> from coola.testers import EqualityTester
+    >>> config = EqualityConfig(tester=EqualityTester())
+    >>> handler = ArrayEqualHandler()
+    >>> handler.handle(np.ones((2, 3)), np.ones((2, 3)), config)
+    True
+    >>> handler.handle(np.ones((2, 3)), np.zeros((2, 3)), config)
+    False
+
+    ```
+    """
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, self.__class__)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__}()"
+
+    def handle(
+        self,
+        object1: np.ndarray,
+        object2: np.ndarray,
+        config: EqualityConfig,
+    ) -> bool | None:
+        object_equal = np.array_equal(object1, object2)
+        if config.show_difference and not object_equal:
+            logger.info(
+                f"numpy.ndarrays have different elements:\n"
+                f"object1=\n{object1}\nobject2=\n{object2}"
+            )
+        return object_equal
+
+    def set_next_handler(self, handler: BaseEqualityHandler) -> None:
+        pass  # Do nothing because the next handler is never called.
---
+++ tests/unit/equality/handlers/test_numpy.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+
+import logging
+from unittest.mock import Mock
+
+import pytest
+
+from coola import EqualityTester
+from coola.equality import EqualityConfig
+from coola.equality.handlers import FalseHandler
+from coola.equality.handlers.numpy_ import ArrayEqualHandler
+from coola.testing import numpy_available
+from coola.utils import is_numpy_available
+
+if is_numpy_available():
+    import numpy as np
+else:
+    np = Mock()
+
+
+@pytest.fixture()
+def config() -> EqualityConfig:
+    return EqualityConfig(tester=EqualityTester())
+
+
+#######################################
+#     Tests for ArrayEqualHandler     #
+#######################################
+
+
+def test_array_equal_handler_eq_true() -> None:
+    assert ArrayEqualHandler() == ArrayEqualHandler()
+
+
+def test_array_equal_handler_eq_false() -> None:
+    assert ArrayEqualHandler() != FalseHandler()
+
+
+def test_array_equal_handler_str() -> None:
+    assert str(ArrayEqualHandler()).startswith("ArrayEqualHandler(")
+
+
+@numpy_available
+@pytest.mark.parametrize(
+    ("object1", "object2"),
+    [
+        (np.ones(shape=(2, 3), dtype=float), np.ones(shape=(2, 3), dtype=float)),
+        (np.ones(shape=(2, 3), dtype=int), np.ones(shape=(2, 3), dtype=int)),
+        (np.ones(shape=(2, 3, 4), dtype=bool), np.ones(shape=(2, 3, 4), dtype=bool)),
+    ],
+)
+def test_array_equal_handler_handle_true(
+    object1: np.ndarray, object2: np.ndarray, config: EqualityConfig
+) -> None:
+    assert ArrayEqualHandler().handle(object1, object2, config)
+
+
+@numpy_available
+@pytest.mark.parametrize(
+    ("object1", "object2"),
+    [
+        (np.ones(shape=(2, 3)), np.ones(shape=(3, 2))),
+        (np.ones(shape=(2, 3)), np.ones(shape=(2, 1))),
+        (np.ones(shape=(2, 3)), np.ones(shape=(2, 3, 1))),
+    ],
+)
+def test_array_equal_handler_handle_false(
+    object1: np.ndarray, object2: np.ndarray, config: EqualityConfig
+) -> None:
+    assert not ArrayEqualHandler().handle(object1, object2, config)
+
+
+@numpy_available
+def test_array_equal_handler_handle_false_show_difference(
+    config: EqualityConfig, caplog: pytest.LogCaptureFixture
+) -> None:
+    config.show_difference = True
+    handler = ArrayEqualHandler()
+    with caplog.at_level(logging.INFO):
+        assert not handler.handle(
+            object1=np.ones(shape=(2, 3)), object2=np.ones(shape=(3, 2)), config=config
+        )
+        assert caplog.messages[0].startswith("numpy.ndarrays have different elements:")
+
+
+def test_array_equal_handler_set_next_handler() -> None:
+    ArrayEqualHandler().set_next_handler(FalseHandler())
