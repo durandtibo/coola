5097059741f8083ba2b3587fff52fed7f45bd36e
Thibaut Durand <durand.tibo@gmail.com>
2024-01-06 17:36:26 -0800

    Add `MappingEqualityComparator` (#373)

--- tests/unit/equality/comparators/test_collection.py
+++ tests/unit/equality/comparators/test_collection.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
 
 import logging
-from collections.abc import Sequence
+from collections.abc import Mapping, Sequence
 from unittest.mock import Mock
 
 import pytest
 
 from coola.equality import EqualityConfig
 from coola.equality.comparators import SequenceEqualityComparator
-from coola.equality.comparators.collection import get_type_comparator_mapping
+from coola.equality.comparators.collection import (
+    MappingEqualityComparator,
+    get_type_comparator_mapping,
+)
 from coola.testers import EqualityTester
-from coola.testing import numpy_available, torch_available
+from coola.testing import numpy_available
 from coola.utils import is_numpy_available
 
 if is_numpy_available():
@@ -22,6 +25,149 @@ else:
 @pytest.fixture()
 def config() -> EqualityConfig:
     return EqualityConfig(tester=EqualityTester())
+
+
+###############################################
+#     Tests for MappingEqualityComparator     #
+###############################################
+
+
+def test_mapping_equality_comparator_str() -> None:
+    assert str(MappingEqualityComparator()) == "MappingEqualityComparator()"
+
+
+@numpy_available
+def test_mapping_equality_comparator__eq__true() -> None:
+    assert MappingEqualityComparator() == MappingEqualityComparator()
+
+
+@numpy_available
+def test_mapping_equality_comparator__eq__false() -> None:
+    assert MappingEqualityComparator() != 123
+
+
+def test_mapping_equality_comparator_clone() -> None:
+    op = MappingEqualityComparator()
+    op_cloned = op.clone()
+    assert op is not op_cloned
+    assert op == op_cloned
+
+
+@pytest.mark.parametrize(
+    ("object1", "object2"),
+    [
+        ({}, {}),
+        ({"a": 1, "b": 2}, {"a": 1, "b": 2}),
+        ({"a": 1, "b": {"k": 1}}, {"a": 1, "b": {"k": 1}}),
+    ],
+)
+def test_mapping_equality_comparator_equal_true(
+    object1: Mapping, object2: Mapping, config: EqualityConfig
+) -> None:
+    assert MappingEqualityComparator().equal(object1, object2, config)
+
+
+@numpy_available
+@pytest.mark.parametrize(
+    ("object1", "object2"),
+    [
+        ({"a": np.ones((2, 3)), "b": np.zeros(2)}, {"a": np.ones((2, 3)), "b": np.zeros(2)}),
+        (
+            {"a": np.ones((2, 3)), "b": {"k": np.zeros(2)}},
+            {"a": np.ones((2, 3)), "b": {"k": np.zeros(2)}},
+        ),
+    ],
+)
+def test_mapping_equality_comparator_equal_true_numpy(
+    object1: Mapping, object2: Mapping, config: EqualityConfig
+) -> None:
+    assert MappingEqualityComparator().equal(object1, object2, config)
+
+
+def test_mapping_equality_comparator_equal_true_same_object(config: EqualityConfig) -> None:
+    obj = [1, 2, 3]
+    assert MappingEqualityComparator().equal(obj, obj, config)
+
+
+@numpy_available
+@pytest.mark.parametrize(
+    ("object1", "object2"),
+    [
+        ({"a": np.ones((2, 3)), "b": np.zeros(2)}, {"a": np.ones((2, 3)), "b": np.ones(2)}),
+        (
+            {"a": np.ones((2, 3)), "b": {"k": np.zeros(2)}},
+            {"a": np.ones((2, 3)), "b": {"k": np.ones(2)}},
+        ),
+    ],
+)
+def test_mapping_equality_comparator_equal_false_numpy(
+    object1: Mapping, object2: Mapping, config: EqualityConfig
+) -> None:
+    assert not MappingEqualityComparator().equal(object1, object2, config)
+
+
+@pytest.mark.parametrize(
+    ("object1", "object2"),
+    [
+        ({"a": 1, "b": 2}, {"a": 1, "b": 3}),
+        ({"a": 1, "b": {"k": 1}}, {"a": 1, "b": {"k": 2}}),
+    ],
+)
+def test_mapping_equality_comparator_equal_false_different_value(
+    object1: Mapping, object2: Mapping, config: EqualityConfig
+) -> None:
+    assert not MappingEqualityComparator().equal(object1, object2, config)
+
+
+def test_mapping_equality_comparator_equal_false_different_value_show_difference(
+    caplog: pytest.LogCaptureFixture, config: EqualityConfig
+) -> None:
+    config.show_difference = True
+    comparator = MappingEqualityComparator()
+    with caplog.at_level(logging.INFO):
+        assert not comparator.equal(
+            object1={"a": 1, "b": 2}, object2={"a": 1, "b": 3}, config=config
+        )
+        assert caplog.messages[-1].startswith("mappings have at least one different value:")
+
+
+@pytest.mark.parametrize(
+    ("object1", "object2"),
+    [
+        ({"a": 1, "b": 2}, {"a": 1, "b": 2, "c": 3}),
+        ({}, {"a": 1, "b": 2}),
+    ],
+)
+def test_mapping_equality_comparator_equal_false_different_length(
+    object1: Mapping, object2: Mapping, config: EqualityConfig
+) -> None:
+    assert not MappingEqualityComparator().equal(object1, object2, config)
+
+
+def test_mapping_equality_comparator_equal_false_different_length_show_difference(
+    caplog: pytest.LogCaptureFixture, config: EqualityConfig
+) -> None:
+    config.show_difference = True
+    comparator = MappingEqualityComparator()
+    with caplog.at_level(logging.INFO):
+        assert not comparator.equal(
+            object1={"a": 1, "b": 2}, object2={"a": 1, "b": 2, "c": 3}, config=config
+        )
+        assert caplog.messages[0].startswith("objects have different lengths:")
+
+
+def test_mapping_equality_comparator_equal_false_different_type(config: EqualityConfig) -> None:
+    assert not MappingEqualityComparator().equal(object1={}, object2=(), config=config)
+
+
+def test_mapping_equality_comparator_equal_different_type_show_difference(
+    caplog: pytest.LogCaptureFixture, config: EqualityConfig
+) -> None:
+    config.show_difference = True
+    comparator = MappingEqualityComparator()
+    with caplog.at_level(logging.INFO):
+        assert not comparator.equal(object1={}, object2=(), config=config)
+        assert caplog.messages[0].startswith("objects have different types:")
 
 
 ################################################
@@ -50,7 +196,6 @@ def test_sequence_equality_comparator_clone() -> None:
     assert op == op_cloned
 
 
-@torch_available
 @pytest.mark.parametrize(
     ("object1", "object2"),
     [
@@ -128,7 +273,6 @@ def test_sequence_equality_comparator_equal_false_different_value_show_differenc
         assert caplog.messages[-1].startswith("sequences have at least one different value:")
 
 
-@torch_available
 @pytest.mark.parametrize(("object1", "object2"), [([1, 2, 3], [1, 2, 3, 4]), ((), ("abc", "def"))])
 def test_sequence_equality_comparator_equal_false_different_length(
     object1: Sequence, object2: Sequence, config: EqualityConfig
@@ -167,7 +311,9 @@ def test_sequence_equality_comparator_equal_different_type_show_difference(
 
 def test_get_type_comparator_mapping() -> None:
     assert get_type_comparator_mapping() == {
+        Mapping: MappingEqualityComparator(),
         Sequence: SequenceEqualityComparator(),
+        dict: MappingEqualityComparator(),
         list: SequenceEqualityComparator(),
         tuple: SequenceEqualityComparator(),
     }
