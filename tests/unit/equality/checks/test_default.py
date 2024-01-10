1b5cac77948a5383ffc7265496d649305e9e1a10
Thibaut Durand <durand.tibo@gmail.com>
2024-01-09 22:36:20 -0800

    Improve default tests (#418)

---
+++ tests/unit/equality/checks/test_default.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING
+
+import pytest
+
+from coola import objects_are_equal
+from tests.unit.equality.comparators.test_default import (
+    DEFAULT_EQUAL,
+    DEFAULT_NOT_EQUAL,
+)
+
+if TYPE_CHECKING:
+    from tests.unit.equality.comparators.utils import ExamplePair
+
+
+@pytest.mark.parametrize("example", DEFAULT_EQUAL)
+@pytest.mark.parametrize("show_difference", [True, False])
+def test_objects_are_equal_true(
+    example: ExamplePair, show_difference: bool, caplog: pytest.LogCaptureFixture
+) -> None:
+    with caplog.at_level(logging.INFO):
+        assert objects_are_equal(example.object1, example.object2, show_difference)
+        assert not caplog.messages
+
+
+@pytest.mark.parametrize("example", DEFAULT_NOT_EQUAL)
+def test_objects_are_equal_false(example: ExamplePair, caplog: pytest.LogCaptureFixture) -> None:
+    with caplog.at_level(logging.INFO):
+        assert not objects_are_equal(example.object1, example.object2)
+        assert not caplog.messages
+
+
+@pytest.mark.parametrize("example", DEFAULT_NOT_EQUAL)
+def test_objects_are_equal_false_show_difference(
+    example: ExamplePair, caplog: pytest.LogCaptureFixture
+) -> None:
+    with caplog.at_level(logging.INFO):
+        assert not objects_are_equal(example.object1, example.object2, show_difference=True)
+        assert caplog.messages[-1].startswith(example.expected_message)
--- tests/unit/equality/comparators/test_default.py
+++ tests/unit/equality/comparators/test_default.py
@@ -9,12 +9,45 @@ from coola.equality import EqualityConfig
 from coola.equality.comparators import DefaultEqualityComparator
 from coola.equality.comparators.default import get_type_comparator_mapping
 from coola.equality.testers import EqualityTester
+from tests.unit.equality.comparators.utils import ExamplePair
 
 
 @pytest.fixture()
 def config() -> EqualityConfig:
     return EqualityConfig(tester=EqualityTester())
 
+
+DEFAULT_EQUAL = [
+    pytest.param(ExamplePair(object1=4.2, object2=4.2), id="float"),
+    pytest.param(ExamplePair(object1=42, object2=42), id="int"),
+    pytest.param(ExamplePair(object1="abc", object2="abc"), id="str"),
+    pytest.param(ExamplePair(object1=True, object2=True), id="bool"),
+    pytest.param(ExamplePair(object1=None, object2=None), id="none"),
+]
+
+
+DEFAULT_NOT_EQUAL = [
+    pytest.param(
+        ExamplePair(object1=42, object2=1, expected_message="objects are different:"),
+        id="different values - int",
+    ),
+    pytest.param(
+        ExamplePair(object1="abc", object2="def", expected_message="objects are different:"),
+        id="different values - str",
+    ),
+    pytest.param(
+        ExamplePair(object1=4.2, object2="meow", expected_message="objects have different types:"),
+        id="float vs str",
+    ),
+    pytest.param(
+        ExamplePair(object1=1.0, object2=1, expected_message="objects have different types:"),
+        id="float vs int",
+    ),
+    pytest.param(
+        ExamplePair(object1=1.0, object2=None, expected_message="objects have different types:"),
+        id="float vs none",
+    ),
+]
 
 ###############################################
 #     Tests for DefaultEqualityComparator     #
@@ -132,6 +165,56 @@ def test_default_equality_comparator_equal_different_type_show_difference(
     with caplog.at_level(logging.INFO):
         assert not comparator.equal(object1=[], object2=(), config=config)
         assert caplog.messages[0].startswith("objects have different types:")
+
+
+@pytest.mark.parametrize("example", DEFAULT_EQUAL)
+def test_default_equality_comparator_equal_yes(
+    example: ExamplePair,
+    config: EqualityConfig,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    comparator = DefaultEqualityComparator()
+    with caplog.at_level(logging.INFO):
+        assert comparator.equal(object1=example.object1, object2=example.object2, config=config)
+        assert not caplog.messages
+
+
+@pytest.mark.parametrize("example", DEFAULT_EQUAL)
+def test_default_equality_comparator_equal_yes_show_difference(
+    example: ExamplePair,
+    config: EqualityConfig,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    config.show_difference = True
+    comparator = DefaultEqualityComparator()
+    with caplog.at_level(logging.INFO):
+        assert comparator.equal(object1=example.object1, object2=example.object2, config=config)
+        assert not caplog.messages
+
+
+@pytest.mark.parametrize("example", DEFAULT_NOT_EQUAL)
+def test_default_equality_comparator_equal_false(
+    example: ExamplePair,
+    config: EqualityConfig,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    comparator = DefaultEqualityComparator()
+    with caplog.at_level(logging.INFO):
+        assert not comparator.equal(object1=example.object1, object2=example.object2, config=config)
+        assert not caplog.messages
+
+
+@pytest.mark.parametrize("example", DEFAULT_NOT_EQUAL)
+def test_default_equality_comparator_equal_false_show_difference(
+    example: ExamplePair,
+    config: EqualityConfig,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    config.show_difference = True
+    comparator = DefaultEqualityComparator()
+    with caplog.at_level(logging.INFO):
+        assert not comparator.equal(object1=example.object1, object2=example.object2, config=config)
+        assert caplog.messages[-1].startswith(example.expected_message)
 
 
 #################################################
