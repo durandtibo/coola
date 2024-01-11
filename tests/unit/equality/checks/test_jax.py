1e92e2981fb54d6be89d3fb07269db91d9406bfa
Thibaut Durand <durand.tibo@gmail.com>
2024-01-11 07:05:47 -0800

    Add `objects_are_close` (#430)

--- tests/unit/equality/checks/test_jax.py
+++ tests/unit/equality/checks/test_jax.py
@@ -5,41 +5,51 @@ from typing import TYPE_CHECKING
 
 import pytest
 
-from coola import objects_are_equal
 from coola.testing import jax_available
+from tests.unit.equality.checks.test_default import COMPARATOR_FUNCTIONS
 from tests.unit.equality.comparators.test_jax import (
     JAX_ARRAY_EQUAL,
     JAX_ARRAY_NOT_EQUAL,
 )
 
 if TYPE_CHECKING:
+    from collections.abc import Callable
+
     from tests.unit.equality.comparators.utils import ExamplePair
 
 
 @jax_available
+@pytest.mark.parametrize("function", COMPARATOR_FUNCTIONS)
 @pytest.mark.parametrize("example", JAX_ARRAY_EQUAL)
 @pytest.mark.parametrize("show_difference", [True, False])
 def test_objects_are_equal_true(
-    example: ExamplePair, show_difference: bool, caplog: pytest.LogCaptureFixture
+    function: Callable,
+    example: ExamplePair,
+    show_difference: bool,
+    caplog: pytest.LogCaptureFixture,
 ) -> None:
     with caplog.at_level(logging.INFO):
-        assert objects_are_equal(example.object1, example.object2, show_difference)
+        assert function(example.object1, example.object2, show_difference)
         assert not caplog.messages
 
 
 @jax_available
+@pytest.mark.parametrize("function", COMPARATOR_FUNCTIONS)
 @pytest.mark.parametrize("example", JAX_ARRAY_NOT_EQUAL)
-def test_objects_are_equal_false(example: ExamplePair, caplog: pytest.LogCaptureFixture) -> None:
+def test_objects_are_equal_false(
+    function: Callable, example: ExamplePair, caplog: pytest.LogCaptureFixture
+) -> None:
     with caplog.at_level(logging.INFO):
-        assert not objects_are_equal(example.object1, example.object2)
+        assert not function(example.object1, example.object2)
         assert not caplog.messages
 
 
 @jax_available
+@pytest.mark.parametrize("function", COMPARATOR_FUNCTIONS)
 @pytest.mark.parametrize("example", JAX_ARRAY_NOT_EQUAL)
 def test_objects_are_equal_false_show_difference(
-    example: ExamplePair, caplog: pytest.LogCaptureFixture
+    function: Callable, example: ExamplePair, caplog: pytest.LogCaptureFixture
 ) -> None:
     with caplog.at_level(logging.INFO):
-        assert not objects_are_equal(example.object1, example.object2, show_difference=True)
+        assert not function(example.object1, example.object2, show_difference=True)
         assert caplog.messages[-1].startswith(example.expected_message)
--- tests/unit/equality/checks/test_pandas.py
+++ tests/unit/equality/checks/test_pandas.py
@@ -5,38 +5,48 @@ from typing import TYPE_CHECKING
 
 import pytest
 
-from coola import objects_are_equal
 from coola.testing import pandas_available
+from tests.unit.equality.checks.test_default import COMPARATOR_FUNCTIONS
 from tests.unit.equality.comparators.test_pandas import PANDAS_EQUAL, PANDAS_NOT_EQUAL
 
 if TYPE_CHECKING:
+    from collections.abc import Callable
+
     from tests.unit.equality.comparators.utils import ExamplePair
 
 
 @pandas_available
+@pytest.mark.parametrize("function", COMPARATOR_FUNCTIONS)
 @pytest.mark.parametrize("example", PANDAS_EQUAL)
 @pytest.mark.parametrize("show_difference", [True, False])
 def test_objects_are_equal_true(
-    example: ExamplePair, show_difference: bool, caplog: pytest.LogCaptureFixture
+    function: Callable,
+    example: ExamplePair,
+    show_difference: bool,
+    caplog: pytest.LogCaptureFixture,
 ) -> None:
     with caplog.at_level(logging.INFO):
-        assert objects_are_equal(example.object1, example.object2, show_difference=show_difference)
+        assert function(example.object1, example.object2, show_difference=show_difference)
         assert not caplog.messages
 
 
 @pandas_available
+@pytest.mark.parametrize("function", COMPARATOR_FUNCTIONS)
 @pytest.mark.parametrize("example", PANDAS_NOT_EQUAL)
-def test_objects_are_equal_false(example: ExamplePair, caplog: pytest.LogCaptureFixture) -> None:
+def test_objects_are_equal_false(
+    function: Callable, example: ExamplePair, caplog: pytest.LogCaptureFixture
+) -> None:
     with caplog.at_level(logging.INFO):
-        assert not objects_are_equal(example.object1, example.object2)
+        assert not function(example.object1, example.object2)
         assert not caplog.messages
 
 
 @pandas_available
+@pytest.mark.parametrize("function", COMPARATOR_FUNCTIONS)
 @pytest.mark.parametrize("example", PANDAS_NOT_EQUAL)
 def test_objects_are_equal_false_show_difference(
-    example: ExamplePair, caplog: pytest.LogCaptureFixture
+    function: Callable, example: ExamplePair, caplog: pytest.LogCaptureFixture
 ) -> None:
     with caplog.at_level(logging.INFO):
-        assert not objects_are_equal(example.object1, example.object2, show_difference=True)
+        assert not function(example.object1, example.object2, show_difference=True)
         assert caplog.messages[-1].startswith(example.expected_message)
