bc4fd01ee9ec6690581e9574487ad795300e6b32
Thibaut Durand <durand.tibo@gmail.com>
2024-01-10 23:54:32 -0800

    Add tolerance support to `JaxArrayEqualityComparator` (#428)

--- tests/unit/equality/handlers/test_jax.py
+++ tests/unit/equality/handlers/test_jax.py
@@ -10,6 +10,7 @@ from coola.equality.handlers import FalseHandler, JaxArrayEqualHandler
 from coola.equality.testers import EqualityTester
 from coola.testing import jax_available
 from coola.utils.imports import is_jax_available
+from tests.unit.equality.comparators.utils import ExamplePair
 
 if is_jax_available():
     import jax.numpy as jnp
@@ -20,6 +21,36 @@ else:  # pragma: no cover
 @pytest.fixture()
 def config() -> EqualityConfig:
     return EqualityConfig(tester=EqualityTester())
+
+
+JAX_ARRAY_TOLERANCE = [
+    # atol
+    pytest.param(
+        ExamplePair(object1=jnp.ones((2, 3)), object2=jnp.full((2, 3), 1.5), atol=1.0),
+        id="atol=1",
+    ),
+    pytest.param(
+        ExamplePair(object1=jnp.ones((2, 3)), object2=jnp.full((2, 3), 1.05), atol=0.1),
+        id="atol=0.1",
+    ),
+    pytest.param(
+        ExamplePair(object1=jnp.ones((2, 3)), object2=jnp.full((2, 3), 1.005), atol=0.01),
+        id="atol=0.01",
+    ),
+    # rtol
+    pytest.param(
+        ExamplePair(object1=jnp.ones((2, 3)), object2=jnp.full((2, 3), 1.5), rtol=1.0),
+        id="rtol=1",
+    ),
+    pytest.param(
+        ExamplePair(object1=jnp.ones((2, 3)), object2=jnp.full((2, 3), 1.05), rtol=0.1),
+        id="rtol=0.1",
+    ),
+    pytest.param(
+        ExamplePair(object1=jnp.ones((2, 3)), object2=jnp.full((2, 3), 1.005), rtol=0.01),
+        id="rtol=0.01",
+    ),
+]
 
 
 ##########################################
@@ -99,6 +130,18 @@ def test_jax_array_equal_handler_handle_false_show_difference(
             object1=jnp.ones(shape=(2, 3)), object2=jnp.ones(shape=(3, 2)), config=config
         )
         assert caplog.messages[0].startswith("jax.numpy.ndarrays have different elements:")
+
+
+@jax_available
+@pytest.mark.parametrize("example", JAX_ARRAY_TOLERANCE)
+def test_jax_array_equal_handler_handle_true_tolerance(
+    example: ExamplePair, config: EqualityConfig
+) -> None:
+    config.atol = example.atol
+    config.rtol = example.rtol
+    assert JaxArrayEqualHandler().handle(
+        object1=example.object1, object2=example.object2, config=config
+    )
 
 
 def test_jax_array_equal_handler_set_next_handler() -> None:
