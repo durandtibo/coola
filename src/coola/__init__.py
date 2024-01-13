2d35275582234b6cd80b3323c134e0e84bedf72d
Thibaut Durand <durand.tibo@gmail.com>
2024-01-13 14:30:13 -0800

    Remove `comparators` and `testers` packages (#447)

--- src/coola/__init__.py
+++ src/coola/__init__.py
@@ -1,13 +1,7 @@
 r"""Contain the main features of the ``coola`` package."""
 
 __all__ = [
-    "AllCloseTester",
-    "BaseAllCloseOperator",
-    "BaseAllCloseTester",
-    "BaseEqualityOperator",
-    "BaseEqualityTester",
     "BaseSummarizer",
-    "EqualityTester",
     "Reduction",
     "Summarizer",
     "objects_are_allclose",
@@ -18,7 +12,6 @@ __all__ = [
     "summary",
 ]
 
-from coola.comparators import BaseAllCloseOperator, BaseEqualityOperator
 from coola.comparison import objects_are_allclose, objects_are_equal
 from coola.reduction import Reduction
 from coola.summarization import summary
@@ -27,10 +20,4 @@ from coola.summarizers import (
     Summarizer,
     set_summarizer_options,
     summarizer_options,
-)
-from coola.testers import (
-    AllCloseTester,
-    BaseAllCloseTester,
-    BaseEqualityTester,
-    EqualityTester,
 )
