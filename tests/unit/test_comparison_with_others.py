7604f49544e36e76b692ad0d5955c6424fa9f60d
Thibaut Durand <durand.tibo@gmail.com>
2024-02-27 22:47:01 -0800

    Update documentation to compare with `torch` and `numpy` (#497)

---
+++ tests/unit/test_comparison_with_others.py
@@ -0,0 +1,85 @@
+from __future__ import annotations
+
+import copy
+from collections import deque
+from typing import Any
+from unittest.mock import Mock
+
+import pytest
+
+from coola.testing import numpy_available, torch_available
+from coola.utils import is_numpy_available, is_torch_available
+
+if is_numpy_available():
+    import numpy as np
+else:
+    np = Mock()
+
+
+if is_torch_available():
+    import torch
+else:
+    torch = Mock()
+
+
+@numpy_available
+@pytest.mark.parametrize(
+    "data",
+    [
+        pytest.param(np.ones((2, 3)), id="numpy ndarray"),
+        pytest.param(
+            {"key1": np.ones((2, 3)), "key2": np.ones((2, 3)), "key3": 42},
+            id="dict with array like objects",
+        ),
+        pytest.param(
+            {"key1": np.ones((2, 3)), "key2": np.ones((2, 3)), "key3": 42, "key4": "abc"},
+            id="dict with string",
+        ),
+        pytest.param([np.ones((2, 3)), np.zeros((2, 3)), 42], id="list with array like objects"),
+        pytest.param(
+            [np.ones((2, 3)), np.zeros((2, 3)), 42, "abc"],
+            id="list with string",
+        ),
+        pytest.param(
+            deque([np.ones((2, 3)), np.zeros((2, 3)), 42]),
+            marks=pytest.mark.xfail,
+            id="deque with array like objects",
+        ),
+    ],
+)
+def test_numpy_assert_equal(data: Any) -> None:
+    np.testing.assert_equal(data, copy.deepcopy(data))
+
+
+@torch_available
+@pytest.mark.parametrize(
+    "data",
+    [
+        pytest.param(torch.ones(2, 3), id="torch Tensor"),
+        pytest.param(
+            torch.nn.utils.rnn.pack_padded_sequence(
+                input=torch.arange(10, dtype=torch.float).view(2, 5),
+                lengths=torch.tensor([5, 3], dtype=torch.long),
+                batch_first=True,
+            ),
+            id="torch PackedSequence",
+        ),
+        pytest.param(
+            {"key1": torch.ones(2, 3), "key2": torch.ones(2, 3), "key3": 42},
+            id="dict with tensor like objects",
+        ),
+        pytest.param(
+            {"key1": torch.ones(2, 3), "key2": torch.ones(2, 3), "key3": 42, "key4": "abc"},
+            marks=pytest.mark.xfail,
+            id="dict with string",
+        ),
+        pytest.param([torch.ones(2, 3), torch.zeros(2, 3), 42], id="list with tensor like objects"),
+        pytest.param(
+            [torch.ones(2, 3), torch.zeros(2, 3), 42, "abc"],
+            marks=pytest.mark.xfail,
+            id="list with string",
+        ),
+    ],
+)
+def test_torch_assert_close(data: Any) -> None:
+    torch.testing.assert_close(data, copy.deepcopy(data))
