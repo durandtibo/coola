caad77bbc3fa639e5a3cba4d568fb0eb1cf7dd7a
Thibaut Durand <durand.tibo@gmail.com>
2024-03-21 06:03:13 -0700

    Update `BaseRandomManager` (#529)

--- tests/unit/random/test_random.py
+++ tests/unit/random/test_random.py
@@ -2,27 +2,44 @@ from __future__ import annotations
 
 import random
 
-from coola.random import RandomRandomSeedSetter
+from coola.random import RandomRandomManager
 
 ############################################
 #     Tests for RandomRandomSeedSetter     #
 ############################################
 
 
-def test_random_random_seed_setter_repr() -> None:
-    assert repr(RandomRandomSeedSetter()).startswith("RandomRandomSeedSetter(")
+def test_random_random_manager_repr() -> None:
+    assert repr(RandomRandomManager()).startswith("RandomRandomManager(")
 
 
-def test_random_random_seed_setter_str() -> None:
-    assert str(RandomRandomSeedSetter()).startswith("RandomRandomSeedSetter(")
+def test_random_random_manager_str() -> None:
+    assert str(RandomRandomManager()).startswith("RandomRandomManager(")
 
 
-def test_random_random_seed_setter_manual_seed() -> None:
-    seed_setter = RandomRandomSeedSetter()
-    seed_setter.manual_seed(42)
+def test_random_random_manager_get_rng_state() -> None:
+    rng = RandomRandomManager()
+    state = rng.get_rng_state()
+    assert isinstance(state, tuple)
+
+
+def test_random_random_manager_manual_seed() -> None:
+    rng = RandomRandomManager()
+    rng.manual_seed(42)
     x1 = random.uniform(0, 1)  # noqa: S311
     x2 = random.uniform(0, 1)  # noqa: S311
-    seed_setter.manual_seed(42)
+    rng.manual_seed(42)
+    x3 = random.uniform(0, 1)  # noqa: S311
+    assert x1 == x3
+    assert x1 != x2
+
+
+def test_random_random_manager_set_rng_state() -> None:
+    rng = RandomRandomManager()
+    state = rng.get_rng_state()
+    x1 = random.uniform(0, 1)  # noqa: S311
+    x2 = random.uniform(0, 1)  # noqa: S311
+    rng.set_rng_state(state)
     x3 = random.uniform(0, 1)  # noqa: S311
     assert x1 == x3
     assert x1 != x2
