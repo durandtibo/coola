c1b0af35e6f10fed368117af308b9be5b1b08d3e
Thibaut Durand <durand.tibo@gmail.com>
2024-03-20 23:24:39 -0700

    Update `TorchRandomManager` (#527)

--- tests/unit/random/test_torch.py
+++ tests/unit/random/test_torch.py
@@ -4,35 +4,45 @@ from unittest.mock import patch
 
 import pytest
 
-from coola.random import TorchRandomSeedSetter
+from coola.random import TorchRandomManager
 from coola.testing import torch_available
 from coola.utils import is_torch_available
 
 if is_torch_available():
     import torch
 
-###########################################
-#     Tests for TorchRandomSeedSetter     #
-###########################################
+########################################
+#     Tests for TorchRandomManager     #
+########################################
 
 
 @torch_available
-def test_torch_random_seed_setter_repr() -> None:
-    assert repr(TorchRandomSeedSetter()).startswith("TorchRandomSeedSetter(")
+def test_torch_random_manager_repr() -> None:
+    assert repr(TorchRandomManager()).startswith("TorchRandomManager(")
 
 
 @torch_available
-def test_torch_random_seed_setter_str() -> None:
-    assert str(TorchRandomSeedSetter()).startswith("TorchRandomSeedSetter(")
+def test_torch_random_manager_str() -> None:
+    assert str(TorchRandomManager()).startswith("TorchRandomManager(")
 
 
 @torch_available
-def test_torch_random_seed_setter_manual_seed() -> None:
-    seed_setter = TorchRandomSeedSetter()
-    seed_setter.manual_seed(42)
+def test_torch_random_manager_get_rng_state() -> None:
+    rng = TorchRandomManager()
+    state = rng.get_rng_state()
+    assert isinstance(state, dict)
+    assert len(state) == 2
+    assert torch.is_tensor(state["torch"])
+    assert isinstance(state["torch.cuda"], list)
+
+
+@torch_available
+def test_torch_random_manager_manual_seed() -> None:
+    rng = TorchRandomManager()
+    rng.manual_seed(42)
     x1 = torch.randn(4, 6)
     x2 = torch.randn(4, 6)
-    seed_setter.manual_seed(42)
+    rng.manual_seed(42)
     x3 = torch.randn(4, 6)
     assert x1.equal(x3)
     assert not x1.equal(x2)
@@ -40,16 +50,28 @@ def test_torch_random_seed_setter_manual_seed() -> None:
 
 @torch_available
 @patch("torch.cuda.is_available", lambda: True)
-def test_torch_random_seed_setter_manual_seed_with_cuda() -> None:
-    seed_setter = TorchRandomSeedSetter()
+def test_torch_random_manager_manual_seed_with_cuda() -> None:
+    rng = TorchRandomManager()
     with patch("torch.cuda.manual_seed_all") as mock_manual_seed_all:
-        seed_setter.manual_seed(42)
+        rng.manual_seed(42)
         mock_manual_seed_all.assert_called_with(42)
 
 
-def test_torch_random_seed_setter_no_torch() -> None:
+@torch_available
+def test_torch_random_manager_set_rng_state() -> None:
+    rng = TorchRandomManager()
+    state = rng.get_rng_state()
+    x1 = torch.randn(4, 6)
+    x2 = torch.randn(4, 6)
+    rng.set_rng_state(state)
+    x3 = torch.randn(4, 6)
+    assert x1.equal(x3)
+    assert not x1.equal(x2)
+
+
+def test_torch_random_manager_no_torch() -> None:
     with (
         patch("coola.utils.imports.is_torch_available", lambda: False),
         pytest.raises(RuntimeError, match="`torch` package is required but not installed."),
     ):
-        TorchRandomSeedSetter()
+        TorchRandomManager()
