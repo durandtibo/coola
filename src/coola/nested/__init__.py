293417b155e0d49b5268a4a352a27352fb86fe31
Thibaut Durand <durand.tibo@gmail.com>
2024-04-01 16:51:30 -0700

    Add conversion functions (#547)

---
+++ src/coola/nested/__init__.py
@@ -0,0 +1,10 @@
+r"""Contain simple operations on nested data structure."""
+
+from __future__ import annotations
+
+__all__ = [
+    "convert_to_dict_of_lists",
+    "convert_to_list_of_dicts",
+]
+
+from coola.nested.conversion import convert_to_dict_of_lists, convert_to_list_of_dicts
