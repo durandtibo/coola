c59f149f07a97d1065c0d12324797c494c49547f
Thibaut Durand <durand.tibo@gmail.com>
2024-06-09 11:25:05 -0700

    Add `EqualHandler` (#601)

--- src/coola/equality/handlers/equal.py
+++ src/coola/equality/handlers/equal.py
@@ -2,7 +2,12 @@ r"""Implement handlers to check the objects are equal."""
 
 from __future__ import annotations
 
-__all__ = ["SupportsEqualNan", "EqualNanHandler"]
+__all__ = [
+    "SupportsEqual",
+    "SupportsEqualNan",
+    "EqualHandler",
+    "EqualNanHandler",
+]
 
 import logging
 from typing import TYPE_CHECKING, Any, Protocol
@@ -12,8 +17,23 @@ from coola.equality.handlers.base import BaseEqualityHandler
 if TYPE_CHECKING:
     from coola.equality.config import EqualityConfig
 
-
 logger = logging.getLogger(__name__)
+
+
+class SupportsEqual(Protocol):
+    r"""Implement a protocol to represent objects with a ``equal``
+    method."""
+
+    def equal(self, other: Any) -> bool:
+        r"""Return ``True`` if the two objects are equal, otherwise
+        ``False``.
+
+        Args:
+            other: Specifies the value to compare with.
+
+        Returns:
+            ``True`` if the two objects are equal, otherwise ``False``
+        """
 
 
 class SupportsEqualNan(Protocol):
@@ -32,6 +52,54 @@ class SupportsEqualNan(Protocol):
         Returns:
             ``True`` if the two objects are equal, otherwise ``False``
         """
+
+
+class EqualHandler(BaseEqualityHandler):
+    r"""Check if the two objects have the same data.
+
+    This handler returns ``False`` if the two objects are different
+    data, otherwise it returns ``True``. The first object must have
+    a ``equal`` attribute which indicates if the two objects are
+    equal or not.
+
+    Example usage:
+
+    ```pycon
+    >>> import math
+    >>> from coola.equality import EqualityConfig
+    >>> from coola.equality.handlers import EqualHandler
+    >>> from coola.equality.testers import EqualityTester
+    >>> class MyFloat:
+    ...     def __init__(self, value: float) -> None:
+    ...         self._value = value
+    ...     def equal(self, other: float) -> bool:
+    ...         return self._value == other
+    ...
+    >>> config = EqualityConfig(tester=EqualityTester())
+    >>> handler = EqualHandler()
+    >>> handler.handle(MyFloat(42), 42, config)
+    True
+    >>> handler.handle(MyFloat(42), float("nan"), config)
+    False
+
+    ```
+    """
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, self.__class__)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__}()"
+
+    def handle(self, actual: SupportsEqual, expected: Any, config: EqualityConfig) -> bool:
+        if not actual.equal(expected):
+            if config.show_difference:
+                logger.info(f"objects are not equal:\nactual:\n{actual}\nexpected:\n{expected}")
+            return False
+        return True
+
+    def set_next_handler(self, handler: BaseEqualityHandler) -> None:
+        pass  # Do nothing because the next handler is never called.
 
 
 class EqualNanHandler(BaseEqualityHandler):
--- tests/unit/equality/handlers/test_equal.py
+++ tests/unit/equality/handlers/test_equal.py
@@ -8,15 +8,24 @@ import pytest
 
 from coola.equality import EqualityConfig
 from coola.equality.handlers import EqualNanHandler, FalseHandler, TrueHandler
+from coola.equality.handlers.equal import EqualHandler
 from coola.equality.testers import EqualityTester
 
 if TYPE_CHECKING:
-    from coola.equality.handlers.equal import SupportsEqualNan
+    from coola.equality.handlers.equal import SupportsEqual, SupportsEqualNan
 
 
 @pytest.fixture()
 def config() -> EqualityConfig:
     return EqualityConfig(tester=EqualityTester())
+
+
+class MyFloat:
+    def __init__(self, value: float) -> None:
+        self._value = value
+
+    def equal(self, other: float) -> bool:
+        return self._value == other
 
 
 class MyFloatNan:
@@ -29,24 +38,89 @@ class MyFloatNan:
         return self._value == other
 
 
+##################################
+#     Tests for EqualHandler     #
+##################################
+
+
+def test_equal_handler_eq_true() -> None:
+    assert EqualHandler() == EqualHandler()
+
+
+def test_equal_handler_eq_false() -> None:
+    assert EqualHandler() != FalseHandler()
+
+
+def test_equal_handler_repr() -> None:
+    assert repr(EqualHandler()).startswith("EqualHandler(")
+
+
+def test_equal_handler_str() -> None:
+    assert str(EqualHandler()).startswith("EqualHandler(")
+
+
+@pytest.mark.parametrize(
+    ("actual", "expected"),
+    [(MyFloat(42), 42), (MyFloat(0), 0)],
+)
+def test_equal_handler_handle_true(
+    actual: SupportsEqual, expected: Any, config: EqualityConfig
+) -> None:
+    assert EqualHandler().handle(actual, expected, config)
+
+
+@pytest.mark.parametrize(
+    ("actual", "expected"),
+    [
+        (MyFloat(42), 1),
+        (MyFloat(0), float("nan")),
+        (MyFloat(float("nan")), float("nan")),
+    ],
+)
+def test_equal_handler_handle_false(
+    actual: SupportsEqual, expected: Any, config: EqualityConfig
+) -> None:
+    assert not EqualHandler().handle(actual, expected, config)
+
+
+def test_equal_handler_handle_false_show_difference(
+    config: EqualityConfig, caplog: pytest.LogCaptureFixture
+) -> None:
+    config.show_difference = True
+    handler = EqualHandler()
+    with caplog.at_level(logging.INFO):
+        assert not handler.handle(actual=MyFloat(42), expected=1, config=config)
+        assert caplog.messages[-1].startswith("objects are not equal:")
+
+
+@pytest.mark.parametrize("equal_nan", [True, False])
+def test_equal_handler_handle_equal_nan(config: EqualityConfig, equal_nan: bool) -> None:
+    config.equal_nan = equal_nan
+    assert not EqualHandler().handle(MyFloat(float("nan")), float("nan"), config)
+
+
+def test_equal_handler_set_next_handler() -> None:
+    EqualHandler().set_next_handler(TrueHandler())
+
+
 #####################################
 #     Tests for EqualNanHandler     #
 #####################################
 
 
-def test_same_data_handler_eq_true() -> None:
+def test_equal_nan_handler_eq_true() -> None:
     assert EqualNanHandler() == EqualNanHandler()
 
 
-def test_same_data_handler_eq_false() -> None:
+def test_equal_nan_handler_eq_false() -> None:
     assert EqualNanHandler() != FalseHandler()
 
 
-def test_same_data_handler_repr() -> None:
+def test_equal_nan_handler_repr() -> None:
     assert repr(EqualNanHandler()).startswith("EqualNanHandler(")
 
 
-def test_same_data_handler_str() -> None:
+def test_equal_nan_handler_str() -> None:
     assert str(EqualNanHandler()).startswith("EqualNanHandler(")
 
 
@@ -54,7 +128,7 @@ def test_same_data_handler_str() -> None:
     ("actual", "expected"),
     [(MyFloatNan(42), 42), (MyFloatNan(0), 0)],
 )
-def test_equal_handler_handle_true(
+def test_equal_nan_handler_handle_true(
     actual: SupportsEqualNan, expected: Any, config: EqualityConfig
 ) -> None:
     assert EqualNanHandler().handle(actual, expected, config)
@@ -68,13 +142,13 @@ def test_equal_handler_handle_true(
         (MyFloatNan(float("nan")), float("nan")),
     ],
 )
-def test_equal_handler_handle_false(
+def test_equal_nan_handler_handle_false(
     actual: SupportsEqualNan, expected: Any, config: EqualityConfig
 ) -> None:
     assert not EqualNanHandler().handle(actual, expected, config)
 
 
-def test_equal_handler_handle_false_show_difference(
+def test_equal_nan_handler_handle_false_show_difference(
     config: EqualityConfig, caplog: pytest.LogCaptureFixture
 ) -> None:
     config.show_difference = True
@@ -85,10 +159,10 @@ def test_equal_handler_handle_false_show_difference(
 
 
 @pytest.mark.parametrize("equal_nan", [True, False])
-def test_equal_handler_handle_equal_nan(config: EqualityConfig, equal_nan: bool) -> None:
+def test_equal_nan_handler_handle_equal_nan(config: EqualityConfig, equal_nan: bool) -> None:
     config.equal_nan = equal_nan
     assert EqualNanHandler().handle(MyFloatNan(float("nan")), float("nan"), config) == equal_nan
 
 
-def test_equal_handler_set_next_handler() -> None:
+def test_equal_nan_handler_set_next_handler() -> None:
     EqualNanHandler().set_next_handler(TrueHandler())
