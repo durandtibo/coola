746a65a54bf2c1deff0e2a0270d277597160663c
Thibaut Durand <durand.tibo@gmail.com>
2025-12-18 14:34:19 -0800

    Improve fallback modules (part 1)

--- src/coola/utils/fallback/pyarrow.py
+++ src/coola/utils/fallback/pyarrow.py
@@ -6,13 +6,18 @@ from __future__ import annotations
 __all__ = ["pyarrow"]
 
 from types import ModuleType
+from typing import Any
 
-# Create a fake pyarrow package
-pyarrow: ModuleType = ModuleType("pyarrow")
+from coola.utils.imports import raise_error_pyarrow_missing
 
 
 class Array:
     r"""Fake Array class."""
 
+    def __init__(self, *args: Any, **kwargs: Any) -> None:  # noqa: ARG002
+        raise_error_pyarrow_missing()
 
+
+# Create a fake pyarrow package
+pyarrow: ModuleType = ModuleType("pyarrow")
 pyarrow.Array = Array
--- src/coola/utils/fallback/torch.py
+++ src/coola/utils/fallback/torch.py
@@ -6,6 +6,29 @@ from __future__ import annotations
 __all__ = ["torch"]
 
 from types import ModuleType
+from typing import Any
+
+from coola.utils.imports import raise_error_torch_missing
+
+
+class PackedSequence:
+    r"""Fake PackedSequence class."""
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:  # noqa: ARG002
+        raise_error_torch_missing()
+
+
+class Tensor:
+    r"""Fake Tensor class."""
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:  # noqa: ARG002
+        raise_error_torch_missing()
+
 
 # Create a fake torch package
 torch: ModuleType = ModuleType("torch")
+torch.nn = ModuleType("torch.nn")
+torch.nn.utils = ModuleType("torch.nn.utils")
+torch.nn.utils.rnn = ModuleType("torch.nn.utils.rnn")
+torch.nn.utils.rnn.PackedSequence = PackedSequence
+torch.Tensor = Tensor
