18f3af0de4091a5665f4396bee8627d67063e589
Thibaut Durand <durand.tibo@gmail.com>
2025-12-18 15:23:42 -0800

    Improve fallback modules (part 4) (#1070)

--- src/coola/utils/fallback/polars.py
+++ src/coola/utils/fallback/polars.py
@@ -6,6 +6,28 @@ from __future__ import annotations
 __all__ = ["polars"]
 
 from types import ModuleType
+from typing import Any
+
+from coola.utils.imports import raise_error_polars_missing
+
+
+class FakeClass:
+    r"""Fake class that raises an error because polars is not installed.
+
+    Args:
+        *args: Positional arguments.
+        **kwargs: Keyword arguments.
+
+    Raises:
+        RuntimeError: polars is required for this functionality.
+    """
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:  # noqa: ARG002
+        raise_error_polars_missing()
+
 
 # Create a fake polars package
 polars: ModuleType = ModuleType("polars")
+polars.DataFrame = FakeClass
+polars.LazyFrame = FakeClass
+polars.Series = FakeClass
