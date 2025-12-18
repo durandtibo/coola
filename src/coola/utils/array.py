bffdb6350b36bc9924c04442a7dbe9e79ce300db
Thibaut Durand <durand.tibo@gmail.com>
2025-12-18 01:22:17 -0800

    Clean `utils` package

--- src/coola/utils/array.py
+++ src/coola/utils/array.py
@@ -5,25 +5,24 @@ from __future__ import annotations
 __all__ = ["to_array"]
 
 from typing import TYPE_CHECKING
-from unittest.mock import Mock
 
 from coola.utils.imports import is_numpy_available, is_torch_available
 
-if is_numpy_available():
+if TYPE_CHECKING or is_numpy_available():
     import numpy as np
-else:
-    np = Mock()  # pragma: no cover
+else:  # pragma: no cover
+    from coola.utils.fallback.numpy import numpy as np
 
-if is_torch_available():
+if TYPE_CHECKING or is_torch_available():
     import torch
 else:  # pragma: no cover
-    torch = Mock()
+    from coola.utils.fallback.torch import torch
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
 
 
-def to_array(data: Sequence | torch.Tensor | np.ndarray) -> np.ndarray:
+def to_array(data: Sequence[int | float] | torch.Tensor | np.ndarray) -> np.ndarray:
     r"""Convert the input to a ``numpy.ndarray``.
 
     Args:
