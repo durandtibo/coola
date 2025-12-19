175c7caebd43c192daca44bdef81104def9273a5
Thibaut Durand <durand.tibo@gmail.com>
2025-12-19 13:28:40 -0800

    Update dependencies (#1075)

--- dev/generate_versions.py
+++ dev/generate_versions.py
@@ -8,10 +8,10 @@ from pathlib import Path
 
 from feu.utils.io import save_json
 from feu.version import (
+    fetch_latest_major_versions,
+    fetch_latest_minor_versions,
     filter_every_n_versions,
     filter_last_n_versions,
-    get_latest_major_versions,
-    get_latest_minor_versions,
     sort_versions,
     unique_versions,
 )
@@ -19,27 +19,27 @@ from feu.version import (
 logger: logging.Logger = logging.getLogger(__name__)
 
 
-def get_package_versions() -> dict[str, list[str]]:
+def fetch_package_versions() -> dict[str, list[str]]:
     r"""Get the versions for each package.
 
     Returns:
         A dictionary with the versions for each package.
     """
-    polars_verions = get_latest_minor_versions("polars", lower="1.0")
-    xarray_verions = get_latest_minor_versions("xarray", lower="2023.1")
+    polars_verions = fetch_latest_minor_versions("polars", lower="1.0")
+    xarray_verions = fetch_latest_minor_versions("xarray", lower="2023.1")
     return {
-        "jax": list(get_latest_minor_versions("jax", lower="0.5")),
-        "numpy": list(get_latest_minor_versions("numpy", lower="1.24")),
-        "packaging": list(get_latest_major_versions("packaging", lower="22.0")),
-        "pandas": list(get_latest_minor_versions("pandas", lower="2.0")),
+        "jax": list(fetch_latest_minor_versions("jax", lower="0.5")),
+        "numpy": list(fetch_latest_minor_versions("numpy", lower="1.24")),
+        "packaging": list(fetch_latest_major_versions("packaging", lower="22.0")),
+        "pandas": list(fetch_latest_minor_versions("pandas", lower="2.0")),
         "polars": sort_versions(
             unique_versions(
                 filter_every_n_versions(polars_verions, n=5)
                 + filter_last_n_versions(polars_verions, n=1)
             )
         ),
-        "pyarrow": list(get_latest_major_versions("pyarrow", lower="11.0")),
-        "torch": list(get_latest_minor_versions("torch", lower="2.0")),
+        "pyarrow": list(fetch_latest_major_versions("pyarrow", lower="11.0")),
+        "torch": list(fetch_latest_minor_versions("torch", lower="2.0")),
         "xarray": sort_versions(
             unique_versions(
                 filter_every_n_versions(xarray_verions, n=3)
@@ -51,7 +51,7 @@ def get_package_versions() -> dict[str, list[str]]:
 
 def main() -> None:
     r"""Generate the package versions and save them in a JSON file."""
-    versions = get_package_versions()
+    versions = fetch_package_versions()
     logger.info(f"{versions=}")
     path = Path(__file__).parent.parent.joinpath("dev/config").joinpath("package_versions.json")
     logger.info(f"Saving package versions to {path}")
