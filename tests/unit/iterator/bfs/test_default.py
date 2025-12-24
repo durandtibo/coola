db4dbaf5ac84e9ea4f4605511e028720db84d1d8
Thibaut Durand <durand.tibo@gmail.com>
2025-12-24 15:44:40 -0800

    Add `DefaultChildFinder` (#1112)

---
+++ tests/unit/iterator/bfs/test_default.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from typing import Any
+
+import pytest
+
+from coola import objects_are_equal
+from coola.iterator.bfs import ChildFinderRegistry, DefaultChildFinder
+from tests.unit.iterator.bfs.helpers import DEFAULT_SAMPLES
+
+########################################
+#     Tests for DefaultChildFinder     #
+########################################
+
+
+def test_default_child_finder_repr() -> None:
+    assert repr(DefaultChildFinder()) == "DefaultChildFinder()"
+
+
+def test_default_child_finder_str() -> None:
+    assert str(DefaultChildFinder()) == "DefaultChildFinder()"
+
+
+@pytest.mark.parametrize(("data", "expected"), DEFAULT_SAMPLES)
+def test_default_child_finder_find_children(data: Any, expected: Any) -> None:
+    assert objects_are_equal(
+        list(DefaultChildFinder().find_children(data, registry=ChildFinderRegistry())), expected
+    )
