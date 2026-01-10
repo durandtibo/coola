6f73902a166039c5f6ee685f2478611fa33ec9d7
Thibaut Durand <durand.tibo@gmail.com>
2026-01-10 15:19:38 -0800

    Clean summarization

--- src/coola/__init__.py
+++ src/coola/__init__.py
@@ -1,15 +1,11 @@
 r"""Contain the main features of the ``coola`` package."""
 
 __all__ = [
-    "BaseSummarizer",
     "Reduction",
-    "Summarizer",
     "__version__",
     "objects_are_allclose",
     "objects_are_allclose",
     "objects_are_equal",
-    "set_summarizer_options",
-    "summarizer_options",
     "summary",
 ]
 
@@ -17,13 +13,6 @@ from importlib.metadata import PackageNotFoundError, version
 
 from coola.comparison import objects_are_allclose, objects_are_equal
 from coola.reduction import Reduction
-from coola.summarization import summary
-from coola.summarizers import (
-    BaseSummarizer,
-    Summarizer,
-    set_summarizer_options,
-    summarizer_options,
-)
 
 try:
     __version__ = version(__name__)
