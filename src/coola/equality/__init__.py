b72ac7b724f64293ff042f76e5639ebda98e8f5c
Thibaut Durand <durand.tibo@gmail.com>
2026-01-12 15:59:14 -0800

    Add `objects_are_equal` (#1249)

--- src/coola/equality/__init__.py
+++ src/coola/equality/__init__.py
@@ -1,1 +1,7 @@
 r"""Contain code to check if two objects are equal or not."""
+
+from __future__ import annotations
+
+__all__ = ["objects_are_allclose", "objects_are_equal"]
+
+from coola.equality.interface import objects_are_allclose, objects_are_equal
