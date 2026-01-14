9a2388dd8c4d0d22c413a0b1587b4a2f51d9ab79
Thibaut Durand <durand.tibo@gmail.com>
2026-01-14 12:52:09 -0800

    Improve `BaseEqualityHandler`

--- src/coola/equality/handler/base.py
+++ src/coola/equality/handler/base.py
@@ -7,6 +7,8 @@ __all__ = ["BaseEqualityHandler"]
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING, TypeVar
 
+from coola.utils.format import str_sequence
+
 if TYPE_CHECKING:
     from coola.equality.config import EqualityConfig
 
@@ -18,9 +20,13 @@ class BaseEqualityHandler(ABC):
 
     A child class needs to implement the following methods:
 
+    - ``equal``
     - ``handle``
 
     A terminal handler has its next handler set to ``None``.
+
+    Args:
+        next_handler: The next handler.
 
     Example:
         ```pycon
@@ -120,6 +126,9 @@ class BaseEqualityHandler(ABC):
 
             ```
         """
+        if handler is None:
+            msg = "The next handler in the chain cannot be None."
+            raise TypeError(msg)
         self.set_next_handler(handler)
         return handler
 
@@ -150,6 +159,34 @@ class BaseEqualityHandler(ABC):
             current = current.chain(handler)
         return current
 
+    def get_chain_length(self) -> int:
+        """Get the total number of handlers in the chain.
+
+        Returns:
+            The total number of handlers in the chain.
+
+        Example:
+            ```pycon
+            >>> from coola.equality.handler import (
+            ...     SameObjectHandler,
+            ...     SameTypeHandler,
+            ...     SameLengthHandler,
+            ...     ObjectEqualHandler,
+            ... )
+            >>> handler = SameObjectHandler()
+            >>> handler.chain_all(SameTypeHandler(), SameLengthHandler(), ObjectEqualHandler())
+            >>> handler.get_chain_length()
+            4
+
+            ```
+        """
+        count = 1
+        current = self.next_handler
+        while current:
+            count += 1
+            current = current.next_handler
+        return count
+
     def set_next_handler(self, handler: BaseEqualityHandler | None) -> None:
         r"""Set the next handler.
 
@@ -167,6 +204,37 @@ class BaseEqualityHandler(ABC):
         """
         self._verify_next_handler(handler)
         self._next_handler = handler
+
+    def visualize_chain(self) -> str:
+        r"""Visualize the current handler chain.
+
+        Returns:
+            A string containing the visualization of the current handler chain.
+
+        Example:
+            ```pycon
+            >>> from coola.equality.handler import (
+            ...     SameObjectHandler,
+            ...     SameTypeHandler,
+            ...     SameLengthHandler,
+            ...     ObjectEqualHandler,
+            ... )
+            >>> handler = SameObjectHandler()
+            >>> handler.chain_all(SameTypeHandler(), SameLengthHandler(), ObjectEqualHandler())
+            >>> print(handler.visualize_chain())
+            (0): SameObjectHandler()
+            (1): SameTypeHandler()
+            (2): SameLengthHandler()
+            (3): ObjectEqualHandler()
+
+            ```
+        """
+        handlers = []
+        current = self
+        while current:
+            handlers.append(current)
+            current = current.next_handler
+        return str_sequence(handlers)
 
     def _handle_next(self, actual: object, expected: object, config: EqualityConfig) -> bool:
         r"""Return the output from the next handler.
@@ -188,21 +256,30 @@ class BaseEqualityHandler(ABC):
         return self._next_handler.handle(actual, expected, config=config)
 
     def _verify_next_handler(self, handler: BaseEqualityHandler | None) -> None:
-        if handler is None:
+        r"""Verify the next handler is valid.
+
+        Args:
+            handler: The next handler.
+
+        Raises:
+            RuntimeError: if the next handler is self.
+            TypeError: if the next handler is not a BaseEqualityHandler.
+        """
+        if not handler:
             return
         if handler is self:
-            msg = "The current handler cannot be its next handler because it creates a cycle"
+            msg = "Cycle detected! the current handler cannot be its next handler"
             raise RuntimeError(msg)
         if not isinstance(handler, BaseEqualityHandler):
             msg = (
-                f"Incorrect type for `handler`. Expected "
-                f"{BaseEqualityHandler.__qualname__} but received {type(handler)}"
+                f"Incorrect type for 'handler'. Expected "
+                f"{BaseEqualityHandler} but received {type(handler)}"
             )
             raise TypeError(msg)
 
     def __repr__(self) -> str:
         if self.next_handler:
-            return f"{self.__class__.__qualname__}(next_handler={self.next_handler})"
+            return f"{self.__class__.__qualname__}(next_handler={self.next_handler!r})"
         return f"{self.__class__.__qualname__}()"
 
     def __str__(self) -> str:
