f47d8e038ec2c16e2a605e220c358e8cda4151a0
Thibaut Durand <durand.tibo@gmail.com>
2026-01-28 09:49:10 -0800

    Add `post_with_automatic_retry`

--- src/coola/experimental/http/__init__.py
+++ src/coola/experimental/http/__init__.py
@@ -2,6 +2,18 @@ r"""Contain functionalities to send HTTP requests with httpx library."""
 
 from __future__ import annotations
 
-__all__ = ["HttpRequestError"]
+__all__ = [
+    "DEFAULT_BACKOFF_FACTOR",
+    "DEFAULT_MAX_RETRIES",
+    "DEFAULT_TIMEOUT",
+    "RETRY_STATUS_CODES",
+    "HttpRequestError",
+]
 
+from coola.experimental.http.constants import (
+    DEFAULT_BACKOFF_FACTOR,
+    DEFAULT_MAX_RETRIES,
+    DEFAULT_TIMEOUT,
+    RETRY_STATUS_CODES,
+)
 from coola.experimental.http.exception import HttpRequestError
