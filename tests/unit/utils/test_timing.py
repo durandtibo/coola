406d1e944713912046333aaf6361984adeefe578
Thibaut Durand <durand.tibo@gmail.com>
2026-04-30 08:13:09 -0700

    Add `TimingResult`

--- tests/unit/utils/test_timing.py
+++ tests/unit/utils/test_timing.py
@@ -2,7 +2,44 @@ from __future__ import annotations
 
 import pytest
 
-from coola.utils.timing import timeblock
+from coola.utils.timing import TimingResult, timeblock
+
+##################################
+#     Tests for TimingResult     #
+##################################
+
+
+def test_timing_result_initial_started_at() -> None:
+    assert TimingResult().started_at is None
+
+
+def test_timing_result_initial_finished_at() -> None:
+    assert TimingResult().finished_at is None
+
+
+def test_timing_result_elapsed_is_none_when_not_started() -> None:
+    assert TimingResult().elapsed is None
+
+
+def test_timing_result_elapsed_is_none_when_only_started() -> None:
+    assert TimingResult(started_at=1000.0).elapsed is None
+
+
+def test_timing_result_elapsed_is_none_when_only_finished() -> None:
+    assert TimingResult(finished_at=1002.0).elapsed is None
+
+
+def test_timing_result_elapsed() -> None:
+    assert TimingResult(started_at=1000.0, finished_at=1002.5).elapsed == 2.5
+
+
+def test_timing_result_elapsed_zero() -> None:
+    assert TimingResult(started_at=1000.0, finished_at=1000.0).elapsed == 0.0
+
+
+def test_timing_result_repr() -> None:
+    assert repr(TimingResult()).startswith("TimingResult(")
+
 
 ###############################
 #     Tests for timeblock     #
