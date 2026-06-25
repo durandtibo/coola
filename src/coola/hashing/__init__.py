4e2ab6fcd05ce3d502540bd246686133de8f1fbf
Thibaut Durand <durand.tibo@gmail.com>
2026-06-24 23:59:18 -0700

    Add `HasherRegistry` (#1516)

--- src/coola/hashing/__init__.py
+++ src/coola/hashing/__init__.py
@@ -2,6 +2,8 @@ r"""Provide deterministic hashing functions."""
 
 from __future__ import annotations
 
-__all__ = ["hash_string"]
+__all__ = ["BaseHasher", "HasherRegistry", "hash_string"]
 
+from coola.hashing.base import BaseHasher
+from coola.hashing.registry import HasherRegistry
 from coola.hashing.string import hash_string
