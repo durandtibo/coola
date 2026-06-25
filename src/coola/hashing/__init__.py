b52aef10e9f35292a46bdbda1262e1d26e8e2d5c
Thibaut Durand <durand.tibo@gmail.com>
2026-06-25 00:39:32 -0700

    Add `MappingHasher` (#1520)

--- src/coola/hashing/__init__.py
+++ src/coola/hashing/__init__.py
@@ -2,10 +2,18 @@ r"""Provide deterministic hashing functions."""
 
 from __future__ import annotations
 
-__all__ = ["BaseHasher", "DefaultHasher", "HasherRegistry", "SequenceHasher", "hash_string"]
+__all__ = [
+    "BaseHasher",
+    "DefaultHasher",
+    "HasherRegistry",
+    "MappingHasher",
+    "SequenceHasher",
+    "hash_string",
+]
 
 from coola.hashing.base import BaseHasher
 from coola.hashing.default import DefaultHasher
+from coola.hashing.mapping import MappingHasher
 from coola.hashing.registry import HasherRegistry
 from coola.hashing.sequence import SequenceHasher
 from coola.hashing.string import hash_string
