1396631957d2b2ea0e2ccab0ee886bb37c8f4cd1
Thibaut Durand <durand.tibo@gmail.com>
2026-06-25 01:14:25 -0700

    Add `hash_object`

--- src/coola/hashing/__init__.py
+++ src/coola/hashing/__init__.py
@@ -10,12 +10,16 @@ __all__ = [
     "MappingHasher",
     "SequenceHasher",
     "StringHasher",
+    "get_default_registry",
+    "hash_object",
     "hash_string",
+    "register_hashers",
 ]
 
 from coola.hashing.base import BaseHasher
 from coola.hashing.datetime import DatetimeHasher
 from coola.hashing.default import DefaultHasher
+from coola.hashing.interface import get_default_registry, hash_object, register_hashers
 from coola.hashing.mapping import MappingHasher
 from coola.hashing.registry import HasherRegistry
 from coola.hashing.sequence import SequenceHasher
