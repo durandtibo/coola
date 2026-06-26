ccee3313e03c9de1f676ff2c08384beffc916299
Thibaut Durand <durand.tibo@gmail.com>
2026-06-25 21:34:42 -0700

    Minor improvements

--- src/coola/utils/imports/universal.py
+++ src/coola/utils/imports/universal.py
@@ -2,7 +2,13 @@ r"""Contain utilities to manage optional dependencies."""
 
 from __future__ import annotations
 
-__all__ = ["check_package", "decorator_package_available", "module_available", "package_available"]
+__all__ = [
+    "check_package",
+    "decorator_package_available",
+    "module_available",
+    "package_available",
+    "raise_package_missing_error",
+]
 
 import importlib
 from contextlib import suppress
