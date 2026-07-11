d381cc37d922490b339437b81024259df24e4f6b
Thibaut Durand <durand.tibo@gmail.com>
2026-07-10 20:21:10 -0700

    Add `hash_pydantic_model` (#1577)

--- src/coola/hashing/__init__.py
+++ src/coola/hashing/__init__.py
@@ -17,6 +17,7 @@ __all__ = [
     "hash_bytes",
     "hash_object",
     "hash_path",
+    "hash_pydantic_model",
     "hash_string",
     "register_hashers",
 ]
@@ -27,6 +28,7 @@ from coola.hashing.datetime import DatetimeHasher
 from coola.hashing.interface import get_default_registry, hash_object, register_hashers
 from coola.hashing.mapping import MappingHasher
 from coola.hashing.path import PathHasher, hash_path
+from coola.hashing.pydantic import hash_pydantic_model
 from coola.hashing.registry import HasherRegistry
 from coola.hashing.repr import ReprHasher
 from coola.hashing.sequence import SequenceHasher
