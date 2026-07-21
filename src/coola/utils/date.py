ad9303da175ea22756ab976d1ac738ba49d890f5
Thibaut Durand <durand.tibo@gmail.com>
2026-07-21 10:49:24 -0700

    Merge date and today (#1600)

--- src/coola/utils/date.py
+++ src/coola/utils/date.py
@@ -2,12 +2,45 @@ r"""Contain utility functions to manipulate dates."""
 
 from __future__ import annotations
 
-__all__ = ["to_date", "to_datetime"]
+__all__ = ["DEFAULT_DATE_FORMAT", "get_today_date", "to_date", "to_datetime"]
 
 from datetime import date, datetime, timezone
+from typing import Final
+from zoneinfo import ZoneInfo
+
+DEFAULT_DATE_FORMAT: Final[str] = "%Y-%m-%d"
 
 
-def to_date(d: date | str, date_format: str = "%Y-%m-%d") -> date:
+def get_today_date(timezone: str = "UTC", date_format: str = DEFAULT_DATE_FORMAT) -> str:
+    r"""Return the current date for the specified timezone.
+
+    Args:
+        timezone: The IANA timezone name to use when determining
+            the current date. Defaults to ``"UTC"``.
+        date_format: A :func:`~datetime.datetime.strftime` format
+            string used to format the returned date.
+            Defaults to ``"%Y-%m-%d"``.
+
+    Returns:
+        A string representing the current date in the given timezone,
+            formatted according to ``date_format``.
+
+    Example:
+        ```pycon
+        >>> from coola.utils.date import get_today_date
+        >>> date = get_today_date()
+        >>> isinstance(date, str)
+        True
+        >>> date = get_today_date(timezone="America/New_York", date_format="%d/%m/%Y")
+        >>> isinstance(date, str)
+        True
+
+        ```
+    """
+    return datetime.now(ZoneInfo(timezone)).strftime(date_format)
+
+
+def to_date(d: date | str, date_format: str = DEFAULT_DATE_FORMAT) -> date:
     r"""Convert the input to a ``date`` object.
 
     Args:
@@ -32,6 +65,7 @@ def to_date(d: date | str, date_format: str = "%Y-%m-%d") -> date:
         ```
     """
     if isinstance(d, str):
+        # tzinfo is only set to satisfy ruff's DTZ007; it is discarded by ``.date()``.
         return datetime.strptime(d, date_format).replace(tzinfo=timezone.utc).date()
     return d
 
