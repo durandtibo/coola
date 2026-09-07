7658b054e3588aa1f84eceaabcd24d16224e160f
Thibaut Durand <durand.tibo@gmail.com>
2026-09-07 16:35:05 -0700

    Improve io

--- src/coola/io/__init__.py
+++ src/coola/io/__init__.py
@@ -6,11 +6,23 @@ __all__ = [
     "BaseFileSaver",
     "BaseLoader",
     "BaseSaver",
+    "JsonLoader",
+    "JsonSaver",
+    "PickleLoader",
+    "PickleSaver",
+    "TextLoader",
+    "TextSaver",
     "add_uuid_suffix",
     "is_loader_config",
     "is_saver_config",
+    "load_json",
+    "load_pickle",
+    "load_text",
     "resolve_loader",
     "resolve_saver",
+    "save_json",
+    "save_pickle",
+    "save_text",
 ]
 
 from coola.io.base import (
@@ -22,4 +34,7 @@ from coola.io.base import (
     resolve_loader,
     resolve_saver,
 )
+from coola.io.json import JsonLoader, JsonSaver, load_json, save_json
+from coola.io.pickle import PickleLoader, PickleSaver, load_pickle, save_pickle
+from coola.io.text import TextLoader, TextSaver, load_text, save_text
 from coola.io.utils import add_uuid_suffix
