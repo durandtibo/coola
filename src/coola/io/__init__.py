e94fa9bbe93a0e74926db0b6af76c80181a0a170
Thibaut Durand <durand.tibo@gmail.com>
2026-09-07 17:24:27 -0700

    Add torch loader/saver

--- src/coola/io/__init__.py
+++ src/coola/io/__init__.py
@@ -12,17 +12,21 @@ __all__ = [
     "PickleSaver",
     "TextLoader",
     "TextSaver",
+    "TorchLoader",
+    "TorchSaver",
     "add_uuid_suffix",
     "is_loader_config",
     "is_saver_config",
     "load_json",
     "load_pickle",
     "load_text",
+    "load_torch",
     "resolve_loader",
     "resolve_saver",
     "save_json",
     "save_pickle",
     "save_text",
+    "save_torch",
 ]
 
 from coola.io.base import (
@@ -37,4 +41,5 @@ from coola.io.base import (
 from coola.io.json import JsonLoader, JsonSaver, load_json, save_json
 from coola.io.pickle import PickleLoader, PickleSaver, load_pickle, save_pickle
 from coola.io.text import TextLoader, TextSaver, load_text, save_text
+from coola.io.torch import TorchLoader, TorchSaver, load_torch, save_torch
 from coola.io.utils import add_uuid_suffix
